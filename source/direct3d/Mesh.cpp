761ed979c6b71a4064017300e2ccb3288d51c676
promit.roy <promit.roy@090eb97a-982f-0410-b69a-795f42cc130f>
2007-05-05 20:54:42 +0000

    Re-added Stream::Close change (accidentally reverted in the move process). Also adding in Mesh, which didn't make it into the last commit.

---
+++ source/direct3d/Mesh.cpp
@@ -0,0 +1,63 @@
+#include <windows.h>
+#include <d3d9.h>
+#include <d3dx9.h>
+#include <vcclr.h>
+
+#include "../DirectXObject.h"
+#include "../Utils.h"
+
+#include "Device.h"
+#include "Mesh.h"
+
+namespace SlimDX
+{
+namespace Direct3D
+{
+	Mesh^ BaseMesh::Clone( Device^ device, MeshFlags flags, array<VertexElement>^ elements )
+	{
+		ID3DXMesh* mesh;
+		pin_ptr<const VertexElement> pinned_elements = &elements[0];
+
+		HRESULT hr = m_BaseMesh->CloneMesh( (DWORD) flags, (const D3DVERTEXELEMENT9*) pinned_elements,
+			device->InternalPointer, &mesh );
+		GraphicsException::CheckHResult( hr );
+
+		return gcnew Mesh( mesh );
+	}
+
+	Mesh::Mesh( ID3DXMesh* mesh ) : BaseMesh( mesh ), m_Mesh( mesh )
+	{
+	}
+
+	Mesh^ Mesh::FromStream( Device^ device, Stream^ stream, MeshFlags flags )
+	{
+		ID3DXMesh* mesh;
+		array<Byte>^ data = Utils::ReadStream( stream, 0 );
+		pin_ptr<unsigned char> pinned_data = &data[0];
+		
+		HRESULT hr = D3DXLoadMeshFromXInMemory( pinned_data, data->Length, (DWORD) flags,
+			device->InternalPointer, NULL, NULL, NULL, NULL, &mesh );
+		GraphicsException::CheckHResult( hr );
+
+		return gcnew Mesh( mesh );
+	}
+
+	Mesh^ Mesh::FromFile( Device^ device, String^ fileName, MeshFlags flags )
+	{
+		ID3DXMesh* mesh;
+		pin_ptr<const wchar_t> pinned_name = PtrToStringChars( fileName );
+
+		HRESULT hr = D3DXLoadMeshFromX( (LPCWSTR) pinned_name, (DWORD) flags, 
+			device->InternalPointer, NULL, NULL, NULL, NULL, &mesh );
+		GraphicsException::CheckHResult( hr );
+
+		return gcnew Mesh( mesh );
+	}
+
+	void Mesh::ComputeTangentFrame( TangentOptions options )
+	{
+		HRESULT hr = D3DXComputeTangentFrame( m_Mesh, (DWORD) options );
+		GraphicsException::CheckHResult( hr );
+	}
+}
+}
