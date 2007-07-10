c03188a0e7505a0a1465a26f2343122638a1ab3b
josh.petrie <josh.petrie@090eb97a-982f-0410-b69a-795f42cc130f>
2007-07-10 23:25:24 +0000

    Some math work; mostly Matrix. Added right-handed methods to fix issue #2.

--- source/math/Matrix.h
+++ source/math/Matrix.h
@@ -39,13 +39,24 @@ namespace SlimDX
 		{
 			Matrix get();
 		}
+		
+		property bool IsIdentity
+		{
+			bool get();
+		}
 
 		void Invert();
 		bool Decompose( [Out] Vector3% scale, [Out] Quaternion% rotation, [Out] Vector3% translation );
 
+		float Determinant();
+
 		static Matrix Invert( Matrix mat );
 		static Matrix Transpose( Matrix mat );
-
+		
+		static Matrix RotationX( float angle );
+		static Matrix RotationY( float angle );
+		static Matrix RotationZ( float angle );
+		static Matrix RotationQuaternion( Quaternion quat );
 		static Matrix RotationAxis( Vector3 axis, float angle );
 		static Matrix RotationYawPitchRoll( float yaw, float pitch, float roll );
 
@@ -56,16 +67,30 @@ namespace SlimDX
 		static Matrix Scaling ( float x, float y, float z );
 		static Matrix Scaling ( Vector3 scaling );
 		static Matrix Scaling ( Vector3% scaling );
-
+		
+		static Matrix AffineTransformation( float scaling, Vector3 rotationCenter, Quaternion rotation, Vector3 translation );
+		static Matrix AffineTransformation2D( float scaling, Vector2 rotationCenter, float rotation, Vector2 translation );
+		static Matrix Transformation( Vector3 scalingCenter, Quaternion scalingRotation, Vector3 scaling, Vector3 rotationCenter, Quaternion rotation, Vector3 translation );
+		static Matrix Transformation2D( Vector2 scalingCenter, float scalingRotation, Vector2 scaling, Vector2 rotationCenter, float rotation, Vector2 translation );
+		
 		static Matrix LookAtLH(Vector3 eye, Vector3 target, Vector3 up );
-
+		static Matrix LookAtRH(Vector3 eye, Vector3 target, Vector3 up );
+		
+		static Matrix OrthoLH( float width, float height, float znear, float zfar );
+		static Matrix OrthoRH( float width, float height, float znear, float zfar );
 		static Matrix OrthoOffCenterLH( float left, float right, float bottom, float top, float znear, float zfar );
+		static Matrix OrthoOffCenterRH( float left, float right, float bottom, float top, float znear, float zfar );
+		static Matrix PerspectiveLH( float width, float height, float znear, float zfar );
+		static Matrix PerspectiveRH( float width, float height, float znear, float zfar );
 		static Matrix PerspectiveFovLH( float fov, float aspect, float znear, float zfar );
-
+		static Matrix PerspectiveFovRH( float fov, float aspect, float znear, float zfar );
+		static Matrix PerspectiveOffCenterLH( float left, float right, float bottom, float top, float znear, float zfar );
+		static Matrix PerspectiveOffCenterRH( float left, float right, float bottom, float top, float znear, float zfar );
+	
 		static Matrix Reflection( Plane plane );
-
-		static Matrix RotationQuaternion( Quaternion quat );
-
+		static Matrix Shadow( Vector4 light, Plane plane );
+		
 		static Matrix operator * ( Matrix lhs, Matrix rhs );
+		
 	};
 }
