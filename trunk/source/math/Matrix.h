50de898c3080da664a7712699781703d6942313d
Mike.Popoloski <Mike.Popoloski@090eb97a-982f-0410-b69a-795f42cc130f>
2007-10-30 21:37:34 +0000

    Upgraded the Matrix class.

--- trunk/source/math/Matrix.h
+++ trunk/source/math/Matrix.h
@@ -26,8 +26,9 @@ using namespace System::Runtime::InteropServices;
 
 namespace SlimDX
 {
+	[Serializable]
 	[StructLayout( LayoutKind::Sequential )]
-	public value class Matrix
+	public value class Matrix : IEquatable<Matrix>
 	{
 	public:
 		property float M11; property float M12; property float M13; property float M14;
@@ -47,50 +48,112 @@ namespace SlimDX
 
 		void Invert();
 		bool Decompose( [Out] Vector3% scale, [Out] Quaternion% rotation, [Out] Vector3% translation );
-
 		float Determinant();
 
-		static Matrix Invert( Matrix mat );
-		static Matrix Transpose( Matrix mat );
-		
+		static Matrix Multiply( Matrix left, Matrix right );
+		static void   Multiply( Matrix% left, Matrix% right, [Out] Matrix% result );
+
+		static Matrix Billboard( Vector3 objectPosition, Vector3 cameraPosition, Vector3 cameraUpVector, Vector3 cameraForwardVector );
+		static void   Billboard( Vector3% objectPosition, Vector3% cameraPosition, Vector3% cameraUpVector, Vector3% cameraForwardVector, [Out] Matrix% result );
+
 		static Matrix RotationX( float angle );
+		static void   RotationX( float angle, [Out] Matrix% result );
+
 		static Matrix RotationY( float angle );
+		static void   RotationY( float angle, [Out] Matrix% result );
+
 		static Matrix RotationZ( float angle );
-		static Matrix RotationQuaternion( Quaternion quat );
+		static void   RotationZ( float angle, [Out] Matrix% result );
+
 		static Matrix RotationAxis( Vector3 axis, float angle );
+		static void   RotationAxis( Vector3% axis, float angle, [Out] Matrix% result );
+
+		static Matrix RotationQuaternion( Quaternion rotation );
+		static void   RotationQuaternion( Quaternion% rotation, [Out] Matrix% result );
+
 		static Matrix RotationYawPitchRoll( float yaw, float pitch, float roll );
+		static void   RotationYawPitchRoll( float yaw, float pitch, float roll, [Out] Matrix% result );
+
+		static Matrix LookAtLH( Vector3 eye, Vector3 target, Vector3 up );
+		static void   LookAtLH( Vector3% eye, Vector3% target, Vector3% up, [Out] Matrix% result );
+
+		static Matrix LookAtRH( Vector3 eye, Vector3 target, Vector3 up );
+		static void   LookAtRH( Vector3% eye, Vector3% target, Vector3% up, [Out] Matrix% result );
+
+		static Matrix OrthoLH( float width, float height, float znear, float zfar );
+		static void   OrthoLH( float width, float height, float znear, float zfar, [Out] Matrix% result );
+
+		static Matrix OrthoRH( float width, float height, float znear, float zfar );
+		static void   OrthoRH( float width, float height, float znear, float zfar, [Out] Matrix% result );
+
+		static Matrix OrthoOffCenterLH( float left, float right, float bottom, float top, float znear, float zfar );
+		static void   OrthoOffCenterLH( float left, float right, float bottom, float top, float znear, float zfar, [Out] Matrix% result );
+
+		static Matrix OrthoOffCenterRH( float left, float right, float bottom, float top, float znear, float zfar );
+		static void   OrthoOffCenterRH( float left, float right, float bottom, float top, float znear, float zfar, [Out] Matrix% result );
+
+		static Matrix PerspectiveLH( float width, float height, float znear, float zfar );
+		static void   PerspectiveLH( float width, float height, float znear, float zfar, [Out] Matrix% result );
+
+		static Matrix PerspectiveRH( float width, float height, float znear, float zfar );
+		static void   PerspectiveRH( float width, float height, float znear, float zfar, [Out] Matrix% result );
+
+		static Matrix PerspectiveFovLH( float fov, float aspect, float znear, float zfar );
+		static void   PerspectiveFovLH( float fov, float aspect, float znear, float zfar, [Out] Matrix% result );
+
+		static Matrix PerspectiveFovRH( float fov, float aspect, float znear, float zfar );
+		static void   PerspectiveFovRH( float fov, float aspect, float znear, float zfar, [Out] Matrix% result );
+
+		static Matrix PerspectiveOffCenterLH( float left, float right, float bottom, float top, float znear, float zfar );
+		static void   PerspectiveOffCenterLH( float left, float right, float bottom, float top, float znear, float zfar, [Out] Matrix% result );
+		
+		static Matrix PerspectiveOffCenterRH( float left, float right, float bottom, float top, float znear, float zfar );
+		static void   PerspectiveOffCenterRH( float left, float right, float bottom, float top, float znear, float zfar, [Out] Matrix% result );
+
+		static Matrix Reflection( Plane plane );
+		static void   Reflection( Plane% plane, [Out] Matrix% result );
+
+		static Matrix Scaling( float x, float y, float z );
+		static void   Scaling( float x, float y, float z, [Out] Matrix% result );
+
+		static Matrix Scaling( Vector3 scale );
+		static void   Scaling( Vector3% scale, [Out] Matrix% result );
+
+		static Matrix Shadow( Vector4 light, Plane plane );
+		static void   Shadow( Vector4% light, Plane% plane, [Out] Matrix% result );
 
 		static Matrix Translation( float x, float y, float z );
+		static void   Translation( float x, float y, float z, [Out] Matrix% result );
+
 		static Matrix Translation( Vector3 amount );
-		static Matrix Translation( Vector3% amount );
-		
-		static Matrix Scaling ( float x, float y, float z );
-		static Matrix Scaling ( Vector3 scale );
-		static Matrix Scaling ( Vector3% scale );
-		
+		static void   Translation( Vector3% amount, [Out] Matrix% result );
+
+		static Matrix Invert( Matrix matrix );
+		static void   Invert( Matrix% matrix, [Out] Matrix% result );
+
+		static Matrix Transpose( Matrix matrix );
+		static void   Transpose( Matrix% matrix, [Out] Matrix% result );
+
 		static Matrix AffineTransformation( float scaling, Vector3 rotationCenter, Quaternion rotation, Vector3 translation );
+		static void   AffineTransformation( float scaling, Vector3% rotationCenter, Quaternion% rotation, Vector3% translation, [Out] Matrix% result );
+		
 		static Matrix AffineTransformation2D( float scaling, Vector2 rotationCenter, float rotation, Vector2 translation );
+		static void   AffineTransformation2D( float scaling, Vector2% rotationCenter, float rotation, Vector2% translation, [Out] Matrix% result );
+
 		static Matrix Transformation( Vector3 scalingCenter, Quaternion scalingRotation, Vector3 scaling, Vector3 rotationCenter, Quaternion rotation, Vector3 translation );
+		static void   Transformation( Vector3% scalingCenter, Quaternion% scalingRotation, Vector3% scaling, Vector3% rotationCenter, Quaternion% rotation, Vector3% translation, [Out] Matrix% result );
+		
 		static Matrix Transformation2D( Vector2 scalingCenter, float scalingRotation, Vector2 scaling, Vector2 rotationCenter, float rotation, Vector2 translation );
+		static void   Transformation2D( Vector2% scalingCenter, float scalingRotation, Vector2% scaling, Vector2% rotationCenter, float rotation, Vector2% translation, [Out] Matrix% result );
 		
-		static Matrix LookAtLH(Vector3 eye, Vector3 target, Vector3 up );
-		static Matrix LookAtRH(Vector3 eye, Vector3 target, Vector3 up );
+		static Matrix operator * ( Matrix left, Matrix right );
+		static bool operator == ( Matrix left, Matrix right );
+		static bool operator != ( Matrix left, Matrix right );
 		
-		static Matrix OrthoLH( float width, float height, float znear, float zfar );
-		static Matrix OrthoRH( float width, float height, float znear, float zfar );
-		static Matrix OrthoOffCenterLH( float left, float right, float bottom, float top, float znear, float zfar );
-		static Matrix OrthoOffCenterRH( float left, float right, float bottom, float top, float znear, float zfar );
-		static Matrix PerspectiveLH( float width, float height, float znear, float zfar );
-		static Matrix PerspectiveRH( float width, float height, float znear, float zfar );
-		static Matrix PerspectiveFovLH( float fov, float aspect, float znear, float zfar );
-		static Matrix PerspectiveFovRH( float fov, float aspect, float znear, float zfar );
-		static Matrix PerspectiveOffCenterLH( float left, float right, float bottom, float top, float znear, float zfar );
-		static Matrix PerspectiveOffCenterRH( float left, float right, float bottom, float top, float znear, float zfar );
-	
-		static Matrix Reflection( Plane plane );
-		static Matrix Shadow( Vector4 light, Plane plane );
-		
-		static Matrix operator * ( Matrix lhs, Matrix rhs );
-		
+		virtual String^ ToString() override;
+		virtual int GetHashCode() override;
+		virtual bool Equals( Object^ obj ) override;
+		virtual bool Equals( Matrix other );
+		static bool Equals( Matrix% value1, Matrix% value2 );
 	};
 }
