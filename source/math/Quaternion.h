62341c25067fcb2da4732a1714d74e48d37d9630
Mike.Popoloski <Mike.Popoloski@090eb97a-982f-0410-b69a-795f42cc130f>
2007-10-31 02:10:39 +0000

    Upgraded the Quaternion class.

--- source/math/Quaternion.h
+++ source/math/Quaternion.h
@@ -24,12 +24,11 @@
 using namespace System;
 using namespace System::Runtime::InteropServices;
 
-#include <d3dx9.h>
-
 namespace SlimDX
 {
+	[Serializable]
 	[StructLayout( LayoutKind::Sequential )]
-	public value class Quaternion
+	public value class Quaternion : IEquatable<Quaternion>
 	{
 	public:
 		property float X;
@@ -37,37 +36,98 @@ namespace SlimDX
 		property float Z;
 		property float W;
 
-		static property Quaternion Identity
-		{
-			Quaternion get();
-		}
+		Quaternion( float x, float y, float z, float w );
+		Quaternion( Vector3 value, float w );
 
-		Quaternion(float x,float y,float z,float w);
+		static property Quaternion Identity { Quaternion get(); }
+		property bool IsIdentity { bool get(); }
+		property Vector3 Axis { Vector3 get(); }
+		property float Angle { float get(); }
 
-		float Magnitude();
-		float MagnitudeSquared();
-		
+		float Length();
+		float LengthSquared();
 		void Normalize();
+		void Conjugate();
+		void Invert();
 
-		static Quaternion Invert( Quaternion quat );
-		static Quaternion Normalize( Quaternion quat );
-		static Quaternion Conjugate( Quaternion quat );
-		static Quaternion Exponential( Quaternion quat );
-		static Quaternion Logarithm( Quaternion quat );
+		static Quaternion Add( Quaternion left, Quaternion right );
+		static void Add( Quaternion% left, Quaternion% right, [Out] Quaternion% result );
+
+		static Quaternion Barycentric( Quaternion source1, Quaternion source2, Quaternion source3, float weight1, float weight2 );
+		static void Barycentric( Quaternion% source1, Quaternion% source2, Quaternion% source3, float weight1, float weight2, [Out] Quaternion% result );
+
+		static Quaternion Concatenate( Quaternion left, Quaternion right );
+		static void Concatenate( Quaternion% left, Quaternion% right, [Out] Quaternion% result );
+
+		static Quaternion Conjugate( Quaternion quaternion );
+		static void Conjugate( Quaternion% quaternion, [Out] Quaternion% result );
+
+		static Quaternion Divide( Quaternion left, Quaternion right );
+		static void Divide( Quaternion% left, Quaternion% right, [Out] Quaternion% result );
+
+		static float Dot( Quaternion left, Quaternion right );
+		static float Dot( Quaternion% left, Quaternion% right );
+
+		static Quaternion Exponential( Quaternion quaternion );
+		static void Exponential( Quaternion% quaternion, [Out] Quaternion% result );
+
+		static Quaternion Invert( Quaternion quaternion );
+		static void Invert( Quaternion% quaternion, [Out] Quaternion% result );
+
+		static Quaternion Lerp( Quaternion left, Quaternion right, float amount );
+		static void Lerp( Quaternion% left, Quaternion% right, float amount, [Out] Quaternion% result );
+
+		static Quaternion Logarithm( Quaternion quaternion );
+		static void Logarithm( Quaternion% quaternion, [Out] Quaternion% result );
+
+		static Quaternion Multiply( Quaternion left, Quaternion right );
+		static void Multiply( Quaternion% left, Quaternion% right, [Out] Quaternion% result );
+
+		static Quaternion Multiply( Quaternion quaternion, float scale );
+		static void Multiply( Quaternion% quaternion, float scale, [Out] Quaternion% result );
+
+		static Quaternion Negate( Quaternion quaternion );
+		static void Negate( Quaternion% quaternion, [Out] Quaternion% result );
+
+		static Quaternion Normalize( Quaternion quaternion );
+		static void Normalize( Quaternion% quaternion, [Out] Quaternion% result );
 
 		static Quaternion RotationAxis( Vector3 axis, float angle );
+		static void RotationAxis( Vector3% axis, float angle, [Out] Quaternion% result );
+
 		static Quaternion RotationMatrix( Matrix matrix );
+		static void RotationMatrix( Matrix% matrix, [Out] Quaternion% result );
+
 		static Quaternion RotationYawPitchRoll( float yaw, float pitch, float roll );
-		static Quaternion BaryCentric( Quaternion q1, Quaternion q2, Quaternion q3, float f, float g );
+		static void RotationYawPitchRoll( float yaw, float pitch, float roll, [Out] Quaternion% result );
 
-		static Quaternion Slerp( Quaternion q1, Quaternion q2, float t);
-		static Quaternion Squad( Quaternion q1, Quaternion a, Quaternion b, Quaternion c, float t );		
+		static Quaternion Slerp( Quaternion left, Quaternion right, float amount );
+		static void Slerp( Quaternion% left, Quaternion% right, float amount, [Out] Quaternion% result );
 
-		static float Dot( Quaternion q1, Quaternion q2 );
-		static Quaternion operator * (Quaternion lhs, Quaternion rhs);
-		static Quaternion operator * (Quaternion lhs, float rhs);
-		static Quaternion operator / (Quaternion lhs, float rhs);
-		static Quaternion operator + (Quaternion lhs, Quaternion rhs);
-		static Quaternion operator - (Quaternion lhs, Quaternion rhs);
+		static Quaternion Squad( Quaternion source1, Quaternion source2, Quaternion source3, Quaternion source4, float amount );
+		static void Squad( Quaternion% source1, Quaternion% source2, Quaternion% source3, Quaternion% source4, float amount, [Out] Quaternion% result );
+
+		static array<Quaternion>^ SquadSetup( Quaternion source1, Quaternion source2, Quaternion source3, Quaternion source4 );
+		static array<Quaternion>^ SquadSetup( Quaternion% source1, Quaternion% source2, Quaternion% source3, Quaternion% source4 );
+
+		static Quaternion Subtract( Quaternion left, Quaternion right );
+		static void Subtract( Quaternion% left, Quaternion% right, [Out] Quaternion% result );
+
+		static Quaternion operator * ( Quaternion left, Quaternion right );
+		static Quaternion operator * ( Quaternion left, float right );
+		static Quaternion operator * ( float left, Quaternion right );
+		static Quaternion operator / ( Quaternion left, float right );
+		static Quaternion operator + ( Quaternion left, Quaternion right );
+		static Quaternion operator - ( Quaternion left, Quaternion right );
+		static Quaternion operator - ( Quaternion quaternion );
+
+		static bool operator == ( Quaternion left, Quaternion right );
+		static bool operator != ( Quaternion left, Quaternion right );
+
+		virtual String^ ToString() override;
+		virtual int GetHashCode() override;
+		virtual bool Equals( Object^ obj ) override;
+		virtual bool Equals( Quaternion other );
+		static bool Equals( Quaternion% value1, Quaternion% value2 );
 	};
 }
