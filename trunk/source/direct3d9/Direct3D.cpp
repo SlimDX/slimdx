6c10715e944e4fe3a2a5d340d9a5f61d6ae2bd17
josh.petrie <josh.petrie@090eb97a-982f-0410-b69a-795f42cc130f>
2007-11-04 15:38:21 +0000

    Update to target the November SDK.

--- trunk/source/direct3d9/Direct3D.cpp
+++ trunk/source/direct3d9/Direct3D.cpp
@@ -22,7 +22,7 @@
 #include <d3d9.h>
 #include <d3dx9.h>
 
-#if D3DX_SDK_VERSION != 35
+#if D3DX_SDK_VERSION != 36
 #error You are not compiling against the August 2007 SDK. Change the linker settings to delay load the correct DLLs and update this code.
 #endif
 
