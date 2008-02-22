7cbf1d50a7d192085b93da810d908cfec577468e
promit.roy <promit.roy@090eb97a-982f-0410-b69a-795f42cc130f>
2008-02-22 22:13:48 +0000

    Removing Saxon and XSL stuff.

--- external/saxon/bin/install-gac.cmd
+++
@@ -1,7 +0,0 @@
-
-				set NET="%PROGRAMFILES%\Microsoft.NET\SDK\v1.1\Bin"
-				%NET%\gacutil /if IKVM.Runtime.dll
-				%NET%\gacutil /if IKVM.GNU.Classpath.dll
-				%NET%\gacutil /if saxon9.dll 
-				%NET%\gacutil /if saxon9api.dll                
-      
