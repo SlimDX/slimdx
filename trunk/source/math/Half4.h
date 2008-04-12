75131dafc2c165d4a2ae06e4153177606a6b08d6
Mike.Popoloski <Mike.Popoloski@090eb97a-982f-0410-b69a-795f42cc130f>
2008-04-12 21:03:55 +0000

    Documented the Color3, Color4, Half, Half2, Half3, Half4, Matrix, and Plane types.

--- trunk/source/math/Half4.h
+++ trunk/source/math/Half4.h
@@ -57,15 +57,64 @@ namespace SlimDX
 		/// <value>The W component of the vector.</value>
 		property Half W;
 
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Half4"/> structure.
+		/// </summary>
+		/// <param name="value">The value to set for the X, Y, Z, and W components.</param>
 		Half4( Half value );
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Half4"/> structure.
+		/// </summary>
+		/// <param name="x">The X component.</param>
+		/// <param name="y">The Y component.</param>
+		/// <param name="z">The Z component.</param>
+		/// <param name="w">The W component.</param>
 		Half4( Half x, Half y, Half z, Half w );
 
+		/// <summary>
+		/// Tests for equality between two objects.
+		/// </summary>
+		/// <param name="left">The first value to compare.</param>
+		/// <param name="right">The second value to compare.</param>
+		/// <returns><c>true</c> if <paramref name="left"/> has the same value as <paramref name="right"/>; otherwise, <c>false</c>.</returns>
 		static bool operator == ( Half4 left, Half4 right );
+
+		/// <summary>
+		/// Tests for inequality between two objects.
+		/// </summary>
+		/// <param name="left">The first value to compare.</param>
+		/// <param name="right">The second value to compare.</param>
+		/// <returns><c>true</c> if <paramref name="left"/> has a different value than <paramref name="right"/>; otherwise, <c>false</c>.</returns>
 		static bool operator != ( Half4 left, Half4 right );
 
+		/// <summary>
+		/// Returns the hash code for this instance.
+		/// </summary>
+		/// <returns>A 32-bit signed integer hash code.</returns>
 		virtual int GetHashCode() override;
+
+		/// <summary>
+		/// Returns a value that indicates whether the current instance is equal to a specified object. 
+		/// </summary>
+		/// <param name="obj">Object to make the comparison with.</param>
+		/// <returns><c>true</c> if the current instance is equal to the specified object; <c>false</c> otherwise.</returns>
 		virtual bool Equals( System::Object^ obj ) override;
+
+		/// <summary>
+		/// Returns a value that indicates whether the current instance is equal to the specified object. 
+		/// </summary>
+		/// <param name="other">Object to make the comparison with.</param>
+		/// <returns><c>true</c> if the current instance is equal to the specified object; <c>false</c> otherwise.</returns>
 		virtual bool Equals( Half4 other );
+
+		/// <summary>
+		/// Determines whether the specified object instances are considered equal. 
+		/// </summary>
+		/// <param name="value1"></param>
+		/// <param name="value2"></param>
+		/// <returns><c>true</c> if <paramref name="value1"/> is the same instance as <paramref name="value2"/> or 
+		/// if both are <c>null</c> references or if <c>value1.Equals(value2)</c> returns <c>true</c>; otherwise, <c>false</c>.</returns>
 		static bool Equals( Half4% value1, Half4% value2 );
 	};
 }
