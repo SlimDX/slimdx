40fe351a560b09e28a25cec4d67cb6bc30c7f53a
Mike.Popoloski <Mike.Popoloski@090eb97a-982f-0410-b69a-795f42cc130f>
2008-05-18 17:35:11 +0000

    Finished documenting the other texture stuff.

--- trunk/source/direct3d9/ImageInformation.h
+++ trunk/source/direct3d9/ImageInformation.h
@@ -27,42 +27,121 @@ namespace SlimDX
 {
 	namespace Direct3D9
 	{
+		/// <summary>
+		/// Contains the description of the contents of an image file.
+		/// </summary>
+		/// <unmanaged>D3DXIMAGE_INFO</unmanaged>
 		[System::Runtime::InteropServices::StructLayout( System::Runtime::InteropServices::LayoutKind::Sequential )]
 		public value class ImageInformation : System::IEquatable<ImageInformation>
 		{
 		public:
+			/// <summary>
+			/// Width of the original image, in pixels.
+			/// </summary>
 			property int Width;
+
+			/// <summary>
+			/// Height of the original image, in pixels.
+			/// </summary>
             property int Height;
+
+			/// <summary>
+			/// Depth of the original image, in pixels.
+			/// </summary>
             property int Depth;
+
+			/// <summary>
+			/// Number of mip levels in the original image.
+			/// </summary>
             property int MipLevels;
+
+			/// <summary>
+			/// The original format of the image.
+			/// </summary>
             property Format Format;
+
+			/// <summary>
+			/// The type of the texture stored in the file.
+			/// </summary>
             property ResourceType ResourceType;
+
+			/// <summary>
+			/// The format of the image file.
+			/// </summary>
             property ImageFileFormat ImageFileFormat;
 
+			/// <summary>
+			/// Retrieves information about an image file.
+			/// </summary>
+			/// <param name="fileName">Name of the image file.</param>
+			/// <returns>Information about the image.</returns>
 			static ImageInformation FromFile( System::String^ fileName );
+
+			/// <summary>
+			/// Retrieves information about an image from memory.
+			/// </summary>
+			/// <param name="memory">A block of memory containing the image.</param>
+			/// <returns>Information about the image.</returns>
 			static ImageInformation FromMemory( array<System::Byte>^ memory );
 
 			/// <summary>
-			/// Function to extract image information from a stream.
+			/// Retrieves information about an image from a stream.
 			/// </summary>
 			/// <param name="stream">Stream containing the image.</param>
-			/// <param name="peek">TRUE to preserve the stream position, FALSE will move the stream pointer.</param>
+			/// <param name="peek"><c>true</c> to preserve the stream position; <c>false</c> will move the stream pointer.</param>
 			/// <returns>Information about the image.</returns>
 			static ImageInformation FromStream(System::IO::Stream^ stream, bool peek);
 
 			/// <summary>
-			/// Function to extract image information from a stream.
+			/// Retrieves information about an image from a stream.
 			/// </summary>
 			/// <param name="stream">Stream containing the image.</param>
 			/// <returns>Information about the image.</returns>
 			static ImageInformation FromStream(System::IO::Stream^ stream);
 
+			/// <summary>
+			/// Tests for equality between two objects.
+			/// </summary>
+			/// <param name="left">The first value to compare.</param>
+			/// <param name="right">The second value to compare.</param>
+			/// <returns><c>true</c> if <paramref name="left"/> has the same value as <paramref name="right"/>; otherwise, <c>false</c>.</returns>
 			static bool operator == ( ImageInformation left, ImageInformation right );
+
+			/// <summary>
+			/// Tests for inequality between two objects.
+			/// </summary>
+			/// <param name="left">The first value to compare.</param>
+			/// <param name="right">The second value to compare.</param>
+			/// <returns><c>true</c> if <paramref name="left"/> has a different value than <paramref name="right"/>; otherwise, <c>false</c>.</returns>
 			static bool operator != ( ImageInformation left, ImageInformation right );
 
+			/// <summary>
+			/// Returns the hash code for this instance.
+			/// </summary>
+			/// <returns>A 32-bit signed integer hash code.</returns>
 			virtual int GetHashCode() override;
+
+			/// <summary>
+			/// Returns a value that indicates whether the current instance is equal to a specified object. 
+			/// </summary>
+			/// <param name="obj">Object to make the comparison with.</param>
+			/// <returns><c>true</c> if the current instance is equal to the specified object; <c>false</c> otherwise.</returns>
 			virtual bool Equals( System::Object^ obj ) override;
+
+			/// <summary>
+			/// Returns a value that indicates whether the current instance is equal to the specified object. 
+			/// </summary>
+			/// <param name="other">Object to make the comparison with.</param>
+			/// <returns><c>true</c> if the current instance is equal to the specified object; <c>false</c> otherwise.</returns>
 			virtual bool Equals( ImageInformation other );
+
+			/// <summary>
+			/// Determines whether the specified object instances are considered equal. 
+			/// </summary>
+			/// <param name="value1">The first value to compare.</param>
+			/// <param name="value2">The second value to compare.</param>
+			/// <returns><c>true</c> if <paramref name="value1"/> is the same instance as <paramref name="value2"/> or 
+			/// if both are <c>null</c> references or if <c>value1.Equals(value2)</c> returns <c>true</c>; otherwise, <c>false</c>.</returns>
 			static bool Equals( ImageInformation% value1, ImageInformation% value2 );
 		};
 	}
--- trunk/source/direct3d9/TextureShader.h
+++ trunk/source/direct3d9/TextureShader.h
@@ -25,48 +25,203 @@ namespace SlimDX
 {
 	namespace Direct3D9
 	{
+		/// <summary>
+		/// Represents a compiled shader designed to fill textures with image data.
+		/// </summary>
 		public ref class TextureShader : public ComObject
 		{
 			COMOBJECT(ID3DXTextureShader, TextureShader);
 
 		public:
+			/// <summary>
+			/// Initializes a new instance of the <see cref="TextureShader"/> class.
+			/// </summary>
+			/// <param name="stream">A stream of compiled shader data.</param>
 			TextureShader( DataStream^ stream );
+
+			/// <summary>
+			/// Constructs a new instance of the <see cref="TextureShader"/> class using the specified pointer to a
+			/// previously constructed unmanaged object.
+			/// </summary>
+			/// <param name="pointer">The unmanaged ID3DXTextureShader pointer.</param>
+			/// <returns>The newly constructed object.</returns>
 			static TextureShader^ FromPointer( System::IntPtr pointer );
 
+			/// <summary>
+			/// Gets a constant by looking up its index.
+			/// </summary>
+			/// <param name="parent">The parent handle.</param>
+			/// <param name="index">The index of the constant.</param>
+			/// <returns>A handle to the constant data.</returns>
 			EffectHandle^ GetConstant( EffectHandle^ parent, int index );
+
+			/// <summary>
+			/// Gets a constant by looking up its name.
+			/// </summary>
+			/// <param name="parent">The parent handle.</param>
+			/// <param name="name">The name of the constant.</param>
+			/// <returns>A handle to the constant data.</returns>
 			EffectHandle^ GetConstant( EffectHandle^ parent, System::String^ name );
+
+			/// <summary>
+			/// Gets a constant from the constant table.
+			/// </summary>
+			/// <param name="handle">The array handle.</param>
+			/// <param name="index">The index of the constant.</param>
+			/// <returns>A handle to the constant data.</returns>
 			EffectHandle^ GetConstantElement( EffectHandle^ handle, int index );
 
+			/// <summary>
+			/// Gets a description for a constant.
+			/// </summary>
+			/// <param name="handle">The constant handle.</param>
+			/// <returns>The description of the constant.</returns>
 			ConstantDescription GetConstantDescription( EffectHandle^ handle );
+
+			/// <summary>
+			/// Gets a list of descriptions for an array constant.
+			/// </summary>
+			/// <param name="handle">The array handle.</param>
+			/// <returns>The list of constant descriptions.</returns>
 			array<ConstantDescription>^ GetConstantDescriptionArray( EffectHandle^ handle );
 
+			/// <summary>
+			/// Sets an effect parameter value.
+			/// </summary>
+			/// <param name="parameter">The effect parameter.</param>
+			/// <param name="value">The parameter value.</param>
+			/// <returns>A <see cref="SlimDX.Result"/> object describing the result of the operation.</returns>
 			Result SetValue( EffectHandle^ parameter, bool value );
+
+			/// <summary>
+			/// Sets an array of effect parameter values.
+			/// </summary>
+			/// <param name="parameter">The effect parameter.</param>
+			/// <param name="valuse">The parameter values.</param>
+			/// <returns>A <see cref="SlimDX.Result"/> object describing the result of the operation.</returns>
 			Result SetValue( EffectHandle^ parameter, array<bool>^ values );
+
+			/// <summary>
+			/// Sets an effect parameter value.
+			/// </summary>
+			/// <param name="parameter">The effect parameter.</param>
+			/// <param name="value">The parameter value.</param>
+			/// <returns>A <see cref="SlimDX.Result"/> object describing the result of the operation.</returns>
 			Result SetValue( EffectHandle^ parameter, int value );
+
+			/// <summary>
+			/// Sets an array of effect parameter values.
+			/// </summary>
+			/// <param name="parameter">The effect parameter.</param>
+			/// <param name="valuse">The parameter values.</param>
+			/// <returns>A <see cref="SlimDX.Result"/> object describing the result of the operation.</returns>
 			Result SetValue( EffectHandle^ parameter, array<int>^ values );
+
+			/// <summary>
+			/// Sets an effect parameter value.
+			/// </summary>
+			/// <param name="parameter">The effect parameter.</param>
+			/// <param name="value">The parameter value.</param>
+			/// <returns>A <see cref="SlimDX.Result"/> object describing the result of the operation.</returns>
 			Result SetValue( EffectHandle^ parameter, float value );
+
+			/// <summary>
+			/// Sets an array of effect parameter values.
+			/// </summary>
+			/// <param name="parameter">The effect parameter.</param>
+			/// <param name="valuse">The parameter values.</param>
+			/// <returns>A <see cref="SlimDX.Result"/> object describing the result of the operation.</returns>
 			Result SetValue( EffectHandle^ parameter, array<float>^ values );
+
+			/// <summary>
+			/// Sets an effect parameter value.
+			/// </summary>
+			/// <param name="parameter">The effect parameter.</param>
+			/// <param name="value">The parameter value.</param>
+			/// <returns>A <see cref="SlimDX.Result"/> object describing the result of the operation.</returns>
 			Result SetValue( EffectHandle^ parameter, Vector4 value );
+
+			/// <summary>
+			/// Sets an array of effect parameter values.
+			/// </summary>
+			/// <param name="parameter">The effect parameter.</param>
+			/// <param name="valuse">The parameter values.</param>
+			/// <returns>A <see cref="SlimDX.Result"/> object describing the result of the operation.</returns>
 			Result SetValue( EffectHandle^ parameter, array<Vector4>^ values );
+
+			/// <summary>
+			/// Sets an effect parameter value.
+			/// </summary>
+			/// <param name="parameter">The effect parameter.</param>
+			/// <param name="value">The parameter value.</param>
+			/// <returns>A <see cref="SlimDX.Result"/> object describing the result of the operation.</returns>
 			Result SetValue( EffectHandle^ parameter, Color4 value );
+
+			/// <summary>
+			/// Sets an array of effect parameter values.
+			/// </summary>
+			/// <param name="parameter">The effect parameter.</param>
+			/// <param name="valuse">The parameter values.</param>
+			/// <returns>A <see cref="SlimDX.Result"/> object describing the result of the operation.</returns>
 			Result SetValue( EffectHandle^ parameter, array<Color4>^ values );
+
+			/// <summary>
+			/// Sets an effect parameter value.
+			/// </summary>
+			/// <param name="parameter">The effect parameter.</param>
+			/// <param name="value">The parameter value.</param>
+			/// <returns>A <see cref="SlimDX.Result"/> object describing the result of the operation.</returns>
 			Result SetValue( EffectHandle^ parameter, Matrix value );
+
+			/// <summary>
+			/// Sets an array of effect parameter values.
+			/// </summary>
+			/// <param name="parameter">The effect parameter.</param>
+			/// <param name="valuse">The parameter values.</param>
+			/// <returns>A <see cref="SlimDX.Result"/> object describing the result of the operation.</returns>
 			Result SetValue( EffectHandle^ parameter, array<Matrix>^ values );
+
+			/// <summary>
+			/// Sets an effect parameter value.
+			/// </summary>
+			/// <param name="parameter">The effect parameter.</param>
+			/// <param name="value">The parameter value.</param>
+			/// <returns>A <see cref="SlimDX.Result"/> object describing the result of the operation.</returns>
 			Result SetValueTranspose( EffectHandle^ parameter, Matrix value );
+
+			/// <summary>
+			/// Sets an array of effect parameter values.
+			/// </summary>
+			/// <param name="parameter">The effect parameter.</param>
+			/// <param name="valuse">The parameter values.</param>
+			/// <returns>A <see cref="SlimDX.Result"/> object describing the result of the operation.</returns>
 			Result SetValueTranspose( EffectHandle^ parameter, array<Matrix>^ values );
 
+			/// <summary>
+			/// Resets constant values to those defined in the shader.
+			/// </summary>
+			/// <returns>A <see cref="SlimDX.Result"/> object describing the result of the operation.</returns>
 			Result SetDefaults();
 
+			/// <summary>
+			/// Gets the constant table description.
+			/// </summary>
 			property ConstantTableDescription Description
 			{
 				ConstantTableDescription get();
 			}
 
+			/// <summary>
+			/// Gets the raw stream of constant data.
+			/// </summary>
 			property DataStream^ ConstantBuffer
 			{
 				DataStream^ get();
 			}
 
+			/// <summary>
+			/// Gets the raw shader stream.
+			/// </summary>
 			property DataStream^ FunctionStream
 			{
 				DataStream^ get();
