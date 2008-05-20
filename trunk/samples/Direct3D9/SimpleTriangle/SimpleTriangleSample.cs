18b90d5eeceafc31ff844bc1876aff38c6bd1461
promit.roy <promit.roy@090eb97a-982f-0410-b69a-795f42cc130f>
2008-05-20 20:44:57 +0000

    Moving everything over to VS 2008.

--- trunk/samples/Direct3D9/SimpleTriangle/SimpleTriangleSample.cs
+++ trunk/samples/Direct3D9/SimpleTriangle/SimpleTriangleSample.cs
@@ -28,6 +28,10 @@ using System.Drawing;
 using SlimDX;
 using System.Windows.Forms;
 
+using System.Diagnostics;
+
+using MD3D = Microsoft.DirectX.Direct3D;
+
 namespace SimpleTriangle
 {
     class SimpleTriangleSample : Sample
@@ -37,9 +41,12 @@ namespace SimpleTriangle
         public SimpleTriangleSample()
         {
             Initialize("SlimDX - Simple Triangle Sample", true, 800, 600);
-        }
 
-        protected override void OnWindowCreated(EventArgs e)
+			Vector3 axis = new Vector3(2.0f, 1.0f, 1.0f);
+			Quaternion q = Quaternion.RotationAxis(axis, 45.0f);
+		}
+
+		protected override void OnWindowCreated(EventArgs e)
         {
             Window.KeyDown += new KeyEventHandler(Window_KeyDown);
         }
