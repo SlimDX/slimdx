35fc3c623b0548123d92619607f78523873b4473
Mike.Popoloski <Mike.Popoloski@090eb97a-982f-0410-b69a-795f42cc130f>
2008-05-24 15:43:04 +0000

    Added a line counter utility.

---
+++ trunk/tools/LineCounter/LineCounter/NodeInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace LineCounter
+{
+    enum FileType
+    {
+        SourceFile,
+        HeaderFile
+    }
+
+    class CountedFile
+    {
+        public string Name;
+        public string Path;
+        public int TotalLines;
+        public int CodeLines;
+        public int Comments;
+        public int BlankLines;
+        public FileType FileType;
+        public int Icon;
+    }
+
+    class NodeInfo
+    {
+        public List<CountedFile> Files = new List<CountedFile>();
+    }
+}
