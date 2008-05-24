77dd401fe3796f5d36f802944f75dc8e1e96759c
josh.petrie <josh.petrie@090eb97a-982f-0410-b69a-795f42cc130f>
2008-05-24 17:39:06 +0000

    Thread pump work.

--- trunk/source/direct3d10/ThreadPumpStatus.h
+++ trunk/source/direct3d10/ThreadPumpStatus.h
@@ -24,99 +24,85 @@
 namespace SlimDX
 {
 	namespace Direct3D10
-	{
-		ref class Buffer;
-		
-		/// <summary>
-		/// Properties defining the way a buffer (containing vertex data) is bound
-		/// to the pipeline for rendering.
-		/// </summary>
-		public value class VertexBufferBinding : System::IEquatable<VertexBufferBinding>
+	{	
+		public value class ThreadPumpStatus : System::IEquatable<ThreadPumpStatus>
 		{
 		private:
-			Buffer^ m_Buffer;
-			int m_Stride;
-			int m_Offset;
-
+			System::UInt32 m_IoQueue;
+			System::UInt32 m_ProcessQueue;
+			System::UInt32 m_DeviceQueue;
+			
+		internal:
+			ThreadPumpStatus( System::UInt32 ioCount, System::UInt32 processCount, System::UInt32 deviceCount );
+			
 		public:
 			/// <summary>
-			/// Gets or sets the buffer being bound.
+			/// Gets the number of items in the IO queue.
 			/// </summary>
-			property Direct3D10::Buffer^ Buffer
+			property System::UInt32 IoQueueCount
 			{
-				Direct3D10::Buffer^ get();
-				void set( Direct3D10::Buffer^ value );
-			}
-
-			/// <summary>
-			/// Gets or sets the stride between vertex elements in the buffer (in bytes).
-			/// </summary>
-			property int Stride
-			{
-				int get();
-				void set( int value );
-			}
-
-			/// <summary>
-			/// Gets or sets the offset from the start of the buffer of the first vertex to use (in bytes).
-			/// </summary>
-			property int Offset
-			{
-				int get();
-				void set( int value );
+				System::UInt32 get();
 			}
 			
 			/// <summary>
-			/// Constructs a new VertexBufferBinding object.
+			/// Gets the number of items in the process queue.
 			/// </summary>
-			/// <param name="buffer">The buffer being bound.</param>
-			/// <param name="stride">The stride between vertex element (in bytes).</param>
-			/// <param name="offset">The offset to the first vertex (in bytes).</param>
-			VertexBufferBinding( Direct3D10::Buffer^ buffer, int stride, int offset );
-
+			property System::UInt32 ProcessQueueCount
+			{
+				System::UInt32 get();
+			}
+			
 			/// <summary>
-			/// Tests for equality between two VertexBufferBindings.
+			/// Gets the number of items in the device queue.
+			/// </summary>
+			property System::UInt32 DeviceQueueCount
+			{
+				System::UInt32 get();
+			}
+			
+			/// <summary>
+			/// Tests for equality between two ThreadPumpStatuses.
 			/// </summary>
 			/// <param name="left">The first value to compare.</param>
 			/// <param name="right">The second value to compare.</param>
 			/// <returns><c>true</c> if <paramref name="left"/> has the same value as <paramref name="right"/>; otherwise, <c>false</c>.</returns>
-			static bool operator == ( VertexBufferBinding left, VertexBufferBinding right );
-
+			static bool operator == ( ThreadPumpStatus left, ThreadPumpStatus right );
+			
 			/// <summary>
-			/// Tests for inequality between two VertexBufferBindings.
+			/// Tests for inequality between two ThreadPumpStatuses.
 			/// </summary>
 			/// <param name="left">The first value to compare.</param>
 			/// <param name="right">The second value to compare.</param>
 			/// <returns><c>true</c> if <paramref name="left"/> has a different value than <paramref name="right"/>; otherwise, <c>false</c>.</returns>
-			static bool operator != ( VertexBufferBinding left, VertexBufferBinding right );
+			static bool operator != ( ThreadPumpStatus left, ThreadPumpStatus right );
 
 			/// <summary>
 			/// Returns the hash code for this instance.
 			/// </summary>
 			/// <returns>A 32-bit signed integer hash code.</returns>
 			virtual int GetHashCode() override;
-
+			
 			/// <summary>
 			/// Returns a value indicating whether this instance is equal to the specified object.
 			/// </summary>
 			/// <param name="obj">An object to compare with this instance.</param>
 			/// <returns><c>true</c> if <paramref name="obj"/> has the same value as this instance; otherwise, <c>false</c>.</returns>
 			virtual bool Equals( System::Object^ obj ) override;
-
+			
 			/// <summary>
 			/// Returns a value indicating whether this instance is equal to the specified object.
 			/// </summary>
-			/// <param name="other">A <see cref="SlimDX::VertexBufferBinding"/> to compare with this instance.</param>
+			/// <param name="other">A <see cref="SlimDX::ThreadPumpStatus"/> to compare with this instance.</param>
 			/// <returns><c>true</c> if <paramref name="other"/> has the same value as this instance; otherwise, <c>false</c>.</returns>
-			virtual bool Equals( VertexBufferBinding other );
-
+			virtual bool Equals( ThreadPumpStatus other );
+			
 			/// <summary>
-			/// Returns a value indicating whether the two VertexBufferBindings are equivalent.
+			/// Returns a value indicating whether the two ThreadPumpStatuses are equivalent.
 			/// </summary>
 			/// <param name="value1">The first value to compare.</param>
 			/// <param name="value2">The second value to compare.</param>
 			/// <returns><c>true</c> if <paramref name="value1"/> has the same value as <paramref name="value2"/>; otherwise, <c>false</c>.</returns>
-			static bool Equals( VertexBufferBinding% value1, VertexBufferBinding% value2 );
+			static bool Equals( ThreadPumpStatus% value1, ThreadPumpStatus% value2 );
 		};
 	}
 };
