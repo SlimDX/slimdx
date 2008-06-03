41588723539790992e1bb746d0a3f15cf2bafe7b
Mike.Popoloski <Mike.Popoloski@090eb97a-982f-0410-b69a-795f42cc130f>
2008-06-03 01:38:17 +0000

    Fixed an amusing StateBlock bug and worked a bit more on the GUI.

--- prototype/Interface/Interface/BlendColor.cs
+++ prototype/Interface/Interface/BlendColor.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using SlimDX;
 using System.Drawing;
+using SlimDX;
 
 namespace Interface
 {
--- prototype/Interface/Interface/ScreenVertex.cs
+++ prototype/Interface/Interface/ScreenVertex.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using SlimDX.Direct3D9;
+﻿using SlimDX.Direct3D9;
 
 namespace Interface
 {
