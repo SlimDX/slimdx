de5da75bf50a1a2a6252ca2510c5ebe07b6f8020
josh.petrie <josh.petrie@090eb97a-982f-0410-b69a-795f42cc130f>
2008-06-03 01:40:23 +0000

    Prototype build task for downgrading to VS 2005.

---
+++ tools/BuildTasks/BuildTasks/DowngradeVCProject.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System.Xml.XPath;
+
+namespace BuildTasks {
+	public class DowngradeVCProject : Task {
+		string inputProject;
+		string outputProject;
+
+		[Required]
+		public string InputProject {
+			get {
+				return inputProject;
+			}
+			set {
+				inputProject = value;
+			}
+		}
+
+		[Required]
+		public string OutputProject {
+			get {
+				return outputProject;
+			}
+			set {
+				outputProject = value;
+			}
+		}
+
+		public override bool Execute() {
+			if (!File.Exists(inputProject)) {
+				Log.LogError(string.Format("Input .vcproj '{0}' does not exist.", inputProject));
+				return false;
+			}
+
+			XPathDocument doc = new XPathDocument(inputProject);
+			XPathNavigator root = doc.CreateNavigator();
+
+			using (FileStream stream = new FileStream(outputProject, FileMode.Create))
+			using (StreamWriter writer = new StreamWriter(stream)) {
+				foreach (XPathNavigator element in root.SelectDescendants(XPathNodeType.Element, true)) {
+					WriteElement(writer, 0, element);
+					break;
+				}
+			}
+
+			return true;
+		}
+
+		static void WriteElement(StreamWriter writer, int indent, XPathNavigator element) {
+			string elementPrefix = new string(' ', indent * 2);
+			string attributePrefix = new string(' ', indent * 2 + 2);
+
+			writer.Write("{0}<{1}", elementPrefix, element.Name);
+
+			foreach (XPathNavigator attribute in element.Select("@*")) {
+				if (element.Name == "VisualStudioProject" && attribute.Name == "Version") {
+					writer.Write("{0}{1}Version=\"8.0\"", Environment.NewLine, attributePrefix );
+				}
+				else if(element.Name == "VisualStudioProject" && attribute.Name == "TargetFrameworkVersion") {
+					// No op.
+				}
+				else {
+					writer.Write("{0}{1}{2}=\"{3}\"", Environment.NewLine, attributePrefix, attribute.Name, attribute.Value);
+				}
+			}
+
+			writer.WriteLine(">");
+			
+			foreach (XPathNavigator child in element.SelectChildren(XPathNodeType.Element)) {
+				WriteElement(writer, indent + 1, child);
+			}
+			
+			writer.WriteLine("{0}</{1}>", elementPrefix, element.Name);
+		}
+	}
+}
