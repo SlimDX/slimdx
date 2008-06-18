e6e741097f9a13c46de8aaccb9392b0d5d8e4bf8
promit.roy <promit.roy@090eb97a-982f-0410-b69a-795f42cc130f>
2008-06-18 19:51:31 +0000

    Adding Particles and SimpleLighting samples.

---
+++ trunk/samples/Direct3D9/SimpleLighting/Camera.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+using SlimDX;
+using SlimDX.Direct3D9;
+using SampleFramework;
+
+namespace SimpleLighting
+{
+    //We have abstracted the view and projection information along with the lighting information 
+    //away in an object of this class.
+    public class Camera
+    {
+        public Vector3 cameraPosition, cameraTarget, cameraUpVector;
+        public Device device;
+        public Cull cullMode;
+        public bool lighting;
+        public float zNearPlane, zFarPlane;
+        public float FOVAngle;
+        Window window;
+        public Color color;
+        int XRES, YRES;
+
+        public Camera(int xres, int yres)
+        {
+            //Initializing with default values for parameters
+            XRES = xres;
+            YRES = yres;
+            cameraPosition = new Vector3(0.0f, 7.0f, 20.0f);
+            cameraTarget = new Vector3(0, 0, 0);
+            cameraUpVector = new Vector3(0, 1.0f, 0);
+            cullMode = Cull.Clockwise;
+            lighting = true;
+            zNearPlane = 0;
+            color = Color.MidnightBlue;
+            zFarPlane = 40.0f;
+            FOVAngle = (float)Math.PI / 4;
+        }
+
+        //Called whenever mouse moved event occurs in order to alter the lighting state
+        public void setupCamera(Device device, Window window, MouseEventArgs e)
+        {
+            //View and Projection Transform setup occurs here
+            this.device = device;
+            this.window = window;
+            device.SetTransform(TransformState.Projection, Matrix.PerspectiveFovLH(FOVAngle,
+                 window.Width / window.Height, zNearPlane, zFarPlane));
+            device.SetTransform(TransformState.View, Matrix.LookAtLH(cameraPosition, cameraTarget,
+                 cameraUpVector));
+            device.SetRenderState(RenderState.Lighting, lighting);
+            device.SetRenderState(RenderState.CullMode, cullMode);
+
+            //Lighting and Material information
+            Light light = new Light();
+            Material material = new Material();
+
+            if (SimpleLighting.pointLight)
+            {
+                light.Type = LightType.Point;
+                light.Range = 100.0f;
+                light.Attenuation0 = 0.05f;
+                light.Attenuation1 = 0.05f;
+                light.Attenuation2 = 0.03f;
+                light.Diffuse = Color.ForestGreen;
+                light.Ambient = Color.GhostWhite;
+            }
+            else
+            {
+                light.Type = LightType.Directional;
+                light.Diffuse = Color.ForestGreen;
+                light.Ambient = Color.GhostWhite;
+            }
+
+            Vector3 vecDirection = new Vector3(e.Location.X - XRES / 2, e.Location.Y - YRES / 2, -8.0f);
+            vecDirection = vecDirection - new Vector3(SimpleLighting.x, SimpleLighting.y, SimpleLighting.z);
+            vecDirection.Normalize();
+            light.Direction = vecDirection;    // set the light direction
+            device.SetLight(0, light);
+            device.EnableLight(0, true);
+            material.Diffuse = Color.FloralWhite;
+            material.Ambient = color;
+            device.Material = material;
+        }
+    }
+}
---
+++ trunk/samples/Direct3D9/SimpleLighting/SimpleLighting.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using SampleFramework;
+using SlimDX.Direct3D9;
+using System.Drawing;
+using SlimDX;
+using System.Windows.Forms;
+
+/***********************************************************************************
+ * Program: Lighting and Mesh Animation Sample for SlimDX                          *
+ * Desc:    This program displays a moving mesh (the teapot) with the mouse pointer*
+ *          acting as a light source.                                              *
+ * Author:  Rohit Jain                                                             *
+ * Dated:   6/3/2008                                                               *
+ * *********************************************************************************/
+
+
+namespace SimpleLighting
+{
+    class SimpleLighting : Sample
+    {
+        public static float x, y, z;//Coordinates of teapot
+        public static bool pointLight = false;
+        //Defines the resolution of the screen
+        const int XRES = 1024, YRES = 768;
+        Mesh mesh;
+        //This variable is used to keep track of the mesh movement and is 
+        //incremented per frame
+        float index = 0;
+        Camera camera;
+
+        public SimpleLighting()
+        {
+            Initialize("Lighting and Mesh Animation Sample for SlimDX", true, XRES, YRES);
+        }
+
+        protected override void OnWindowCreated(EventArgs e)
+        {
+            Window.KeyDown += new KeyEventHandler(Window_KeyDown);
+            Window.MouseMove += new MouseEventHandler(Window_MouseMove);
+            Window.MouseClick += new MouseEventHandler(Window_MouseClick);
+        }
+
+        void Window_MouseClick(object sender, MouseEventArgs e)
+        {
+            pointLight = !pointLight;
+        }
+
+        void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            //Enables Escape Key Exit
+            if (e.KeyCode == Keys.Escape)
+            {
+                Window.Close();
+                OnDeviceDestroyed(EventArgs.Empty);
+            }
+        }
+
+        void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            //We refresh the camera to align the light source with the mouse pointer
+            if (camera == null) camera = new Camera(XRES, YRES);
+            camera.setupCamera(Device, Window, e);
+        }
+
+        protected override void OnDeviceLost(EventArgs e)
+        {
+            mesh.Dispose();
+        }
+
+        protected override void OnDeviceReset(EventArgs e)
+        {
+            camera = new Camera(XRES, YRES);
+            mesh = Mesh.CreateTeapot(Device);
+        }
+
+        protected override void OnMainLoop(EventArgs e)
+        {
+            Device.Clear(ClearFlags.Target | ClearFlags.ZBuffer, Color.Black, 1.0f, 0);
+            Device.BeginScene();
+            mesh.ComputeNormals();
+            //Setting up world transformation matrix
+            Matrix matTranslate;
+            matTranslate = Matrix.Identity;
+            //Can change speed of teapot from here
+            index += 0.05f;
+            //Putting in place some complicated movements
+            x = 5.0f * (float)Math.Sin(0.4f * index);
+            y = 3.0f * (float)Math.Sin(0.8f * index);
+            z = 3.0f * (float)Math.Cos(0.16f * index);
+            matTranslate.M41 = x;
+            matTranslate.M42 = y;
+            matTranslate.M43 = z;
+            Device.SetTransform(TransformState.World, matTranslate);
+
+            mesh.DrawSubset(0);
+            Device.EndScene();
+
+            if (Device.Present() == ResultCode.DeviceLost)
+                IsDeviceLost = true;
+        }
+    }
+}
