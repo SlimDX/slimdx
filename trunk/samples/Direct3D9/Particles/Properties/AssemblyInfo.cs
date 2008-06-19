63675f17e05316367b4f156608daef2cf3da1552
Mike.Popoloski <Mike.Popoloski@090eb97a-982f-0410-b69a-795f42cc130f>
2008-06-19 16:41:46 +0000

    Fixed a few FxCop issues.

--- trunk/samples/Direct3D9/Particles/Properties/AssemblyInfo.cs
+++ trunk/samples/Direct3D9/Particles/Properties/AssemblyInfo.cs
@@ -1,16 +1,18 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Particles")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A SlimDX sample showing a simple particle engine at work.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("et")]
-[assembly: AssemblyProduct("WindowsFormsApplication1")]
-[assembly: AssemblyCopyright("Copyright © et 2008")]
+[assembly: AssemblyCompany("SlimDX Group")]
+[assembly: AssemblyProduct("Particles")]
+[assembly: AssemblyCopyright("Copyright © 2008 SlimDX Group")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -18,6 +20,10 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+
+// Setting CLSCompliant to true allows the types in this assembly to be
+// used by other CLS compliant languages, such as VB.Net.
+[assembly: CLSCompliant(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("b818edc7-bf9c-4871-8c1d-498c61576446")]
@@ -34,3 +40,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
--- trunk/samples/Direct3D9/SimpleLighting/Properties/AssemblyInfo.cs
+++ trunk/samples/Direct3D9/SimpleLighting/Properties/AssemblyInfo.cs
@@ -1,16 +1,18 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SimpleLighting")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A SlimDX sample showing simple Direct3D9 lighting.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Day 1 Studios")]
-[assembly: AssemblyProduct("SimpleLighting.Properties")]
-[assembly: AssemblyCopyright("Copyright © Day 1 Studios 2008")]
+[assembly: AssemblyCompany("SlimDX Group")]
+[assembly: AssemblyProduct("SimpleLighting")]
+[assembly: AssemblyCopyright("Copyright © 2008 SlimDX Group")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -18,6 +20,10 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+
+// Setting CLSCompliant to true allows the types in this assembly to be
+// used by other CLS compliant languages, such as VB.Net.
+[assembly: CLSCompliant(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("101808cf-7412-42ef-87e3-9786ec2d921f")]
@@ -31,3 +37,4 @@ using System.Runtime.InteropServices;
 //
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
