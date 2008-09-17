6b45e3a5c7ef3819e86113bfb267c1b6e75bc648
Mike.Popoloski <Mike.Popoloski@090eb97a-982f-0410-b69a-795f42cc130f>
2008-09-17 03:16:20 +0000

    Started cleaning up and refactoring the SampleFramework.

--- samples/SampleFramework/Win32/NativeMethods.cs
+++ samples/SampleFramework/Win32/NativeMethods.cs
@@ -26,201 +26,89 @@ using System.Security;
 
 namespace SampleFramework
 {
-    /// <summary>
-    /// Contains imported native methods.
-    /// </summary>
     static class NativeMethods
     {
-        /// <summary>
-        /// Peeks at the next message in the queue.
-        /// </summary>
-        /// <param name="message">The message.</param>
-        /// <param name="hwnd">The window handle.</param>
-        /// <param name="messageFilterMin">The message filter minimum.</param>
-        /// <param name="messageFilterMax">The message filter maximum.</param>
-        /// <param name="flags">The flags.</param>
-        /// <returns><c>true</c> on success; otherwise, <c>false</c>.</returns>
         [SuppressUnmanagedCodeSecurityAttribute]
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = false)]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool PeekMessage(out NativeMessage message, IntPtr hwnd, uint messageFilterMin, uint messageFilterMax, uint flags);
 
-        /// <summary>
-        /// Gets a window's client rectangle.
-        /// </summary>
-        /// <param name="hWnd">The window handle.</param>
-        /// <param name="lpRect">The client rectangle.</param>
-        /// <returns><c>true</c> on success; otherwise, <c>false</c>.</returns>
         [SuppressUnmanagedCodeSecurityAttribute]
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
         static extern bool GetClientRect(IntPtr hWnd, out NativeRectangle lpRect);
 
-        /// <summary>
-        /// Gets a window's outer rectangle.
-        /// </summary>
-        /// <param name="hWnd">The window handle.</param>
-        /// <param name="lpRect">The outer rectangle.</param>
-        /// <returns><c>true</c> on success; otherwise, <c>false</c>.</returns>
         [SuppressUnmanagedCodeSecurityAttribute]
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
         static extern bool GetWindowRect(IntPtr hWnd, out NativeRectangle lpRect);
 
-        /// <summary>
-        /// Gets a window's placement information.
-        /// </summary>
-        /// <param name="hWnd">The window handle.</param>
-        /// <param name="lpwndpl">The placement information.</param>
-        /// <returns><c>true</c> on success; otherwise, <c>false</c>.</returns>
         [SuppressUnmanagedCodeSecurityAttribute]
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool GetWindowPlacement(IntPtr hWnd, ref WINDOWPLACEMENT lpwndpl);
 
-        /// <summary>
-        /// Sets a window's placement information.
-        /// </summary>
-        /// <param name="hWnd">The window handle.</param>
-        /// <param name="lpwndpl">The placement information.</param>
-        /// <returns><c>true</c> on success; otherwise, <c>false</c>.</returns>
         [SuppressUnmanagedCodeSecurityAttribute]
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool SetWindowPlacement(IntPtr hWnd, ref WINDOWPLACEMENT lpwndpl);
 
-        /// <summary>
-        /// Sets a window style.
-        /// </summary>
-        /// <param name="hWnd">The window handle.</param>
-        /// <param name="nIndex">Index of the style.</param>
-        /// <param name="dwNewLong">The new style value.</param>
-        /// <returns>The old style value.</returns>
         [SuppressUnmanagedCodeSecurityAttribute]
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         public static extern uint SetWindowLong(IntPtr hWnd, int nIndex, uint dwNewLong);
 
-        /// <summary>
-        /// Gets a window style.
-        /// </summary>
-        /// <param name="hWnd">The window handle.</param>
-        /// <param name="nIndex">Index of the style.</param>
-        /// <returns>The style value.</returns>
         [SuppressUnmanagedCodeSecurityAttribute]
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         public static extern uint GetWindowLong(IntPtr hWnd, int nIndex);
 
-        /// <summary>
-        /// Determines whether the specified window is iconic.
-        /// </summary>
-        /// <param name="hWnd">The window handle.</param>
-        /// <returns>
-        /// <c>true</c> if the specified window is iconic; otherwise, <c>false</c>.
-        /// </returns>
         [SuppressUnmanagedCodeSecurityAttribute]
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = false)]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool IsIconic(IntPtr hWnd);
 
-        /// <summary>
-        /// Determines whether the specified window is zoomed.
-        /// </summary>
-        /// <param name="hWnd">The window handle.</param>
-        /// <returns>
-        /// <c>true</c> if the specified window is zoomed; otherwise, <c>false</c>.
-        /// </returns>
         [SuppressUnmanagedCodeSecurityAttribute]
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = false)]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool IsZoomed(IntPtr hWnd);
 
-        /// <summary>
-        /// Shows the window.
-        /// </summary>
-        /// <param name="hWnd">The window handle.</param>
-        /// <param name="nCmdShow">Show flags.</param>
-        /// <returns><c>true</c> on success; otherwise, <c>false</c>.</returns>
         [SuppressUnmanagedCodeSecurityAttribute]
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = false)]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
 
-        /// <summary>
-        /// Adjusts a window rectangle.
-        /// </summary>
-        /// <param name="lpRect">The rectangle.</param>
-        /// <param name="dwStyle">Style flags..</param>
-        /// <param name="bMenu">if set to <c>true</c>, include the menu in calculations as well.</param>
-        /// <returns><c>true</c> on success; otherwise, <c>false</c>.</returns>
         [SuppressUnmanagedCodeSecurityAttribute]
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool AdjustWindowRect(ref NativeRectangle lpRect, uint dwStyle, [MarshalAs(UnmanagedType.Bool)]bool bMenu);
 
-        /// <summary>
-        /// Sets the state of the thread execution.
-        /// </summary>
-        /// <param name="esFlags">The execution flags.</param>
-        /// <returns>The thread execution state.</returns>
         [SuppressUnmanagedCodeSecurityAttribute]
         [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = false)]
         public static extern uint SetThreadExecutionState(uint esFlags);
 
-        /// <summary>
-        /// Sets a window's position..
-        /// </summary>
-        /// <param name="hWnd">The window handle.</param>
-        /// <param name="hWndInsertAfter">The Z-order for the window.</param>
-        /// <param name="X">The X position.</param>
-        /// <param name="Y">The Y position.</param>
-        /// <param name="cx">The width.</param>
-        /// <param name="cy">The height.</param>
-        /// <param name="uFlags">Show flags.</param>
-        /// <returns><c>true</c> on success; otherwise, <c>false</c>.</returns>
         [SuppressUnmanagedCodeSecurityAttribute]
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
 
-        /// <summary>
-        /// Gets the monitor in which the specified Window resides.
-        /// </summary>
-        /// <param name="hwnd">The window handle.</param>
-        /// <param name="dwFlags">The monitor flags.</param>
-        /// <returns>A handle to the monitor.</returns>
         [SuppressUnmanagedCodeSecurityAttribute]
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = false)]
         public static extern IntPtr MonitorFromWindow(IntPtr hwnd, uint dwFlags);
 
-        /// <summary>
-        /// Gets the client rectangle of a window.
-        /// </summary>
-        /// <param name="handle">The handle of the window.</param>
-        /// <returns>The window's client rectangle.</returns>
         public static Rectangle GetClientRectangle(IntPtr handle)
         {
-            // grab the native rectangle
             NativeRectangle rect;
             if (!GetClientRect(handle, out rect))
                 return Rectangle.Empty;
 
-            // return the converted rectangle
             return Rectangle.FromLTRB(rect.left, rect.top, rect.right, rect.bottom);
         }
 
-        /// <summary>
-        /// Gets the outer rectangle of a window.
-        /// </summary>
-        /// <param name="handle">The handle of the window.</param>
-        /// <returns>The window's outer rectangle.</returns>
         public static Rectangle GetWindowRectangle(IntPtr handle)
         {
-            // grab the native rectangle
             NativeRectangle rect;
             if (!GetWindowRect(handle, out rect))
                 return Rectangle.Empty;
 
-            // return the converted rectangle
             return Rectangle.FromLTRB(rect.left, rect.top, rect.right, rect.bottom);
         }
     }
--- samples/SampleFramework/Win32/WindowConstants.cs
+++ samples/SampleFramework/Win32/WindowConstants.cs
@@ -23,174 +23,52 @@ using System;
 
 namespace SampleFramework
 {
-    /// <summary>
-    /// Contains constants for Window operations.
-    /// </summary>
     static class WindowConstants
     {
-        /// <summary>
-        /// A window's size is changing.
-        /// </summary>
         public const int WM_SIZE = 0x5;
-
-        /// <summary>
-        /// A system command was sent to the window.
-        /// </summary>
         public const int WM_SYSCOMMAND = 0x112;
-
-        /// <summary>
-        /// The application is being activated or deactivated.
-        /// </summary>
         public const int WM_ACTIVATEAPP = 0x001C;
-
-        /// <summary>
-        /// A power broadcast message is being sent.
-        /// </summary>
         public const int WM_POWERBROADCAST = 0x0218;
 
-        /// <summary>
-        /// Screensaver is about to activate.
-        /// </summary>
         public const int SC_SCREENSAVE = 0xF140;
-
-        /// <summary>
-        /// Monitor is about to power cycle.
-        /// </summary>
         public const int SC_MONITORPOWER = 0xF170;
 
-        /// <summary>
-        /// Represents the left Windows key.
-        /// </summary>
         public const int VK_LWIN = 0x5B;
-
-        /// <summary>
-        /// Represents the right Windows key.
-        /// </summary>
         public const int VK_RWIN = 0x5C;
 
-        /// <summary>
-        /// The window is being minimized.
-        /// </summary>
         public static readonly IntPtr SIZE_MINIMIZED = new IntPtr(1);
-
-        /// <summary>
-        /// The window is being maximized.
-        /// </summary>
         public static readonly IntPtr SIZE_MAXIMIZED = new IntPtr(2);
-
-        /// <summary>
-        /// The window is being restored.
-        /// </summary>
         public static readonly IntPtr SIZE_RESTORED = new IntPtr(0);
 
-        /// <summary>
-        /// The system is going into suspended mode.
-        /// </summary>
         public static readonly IntPtr PBT_APMQUERYSUSPEND = new IntPtr(0x0000);
-
-        /// <summary>
-        /// The system is coming out of suspended mode.
-        /// </summary>
         public static readonly IntPtr PBT_APMRESUMESUSPEND = new IntPtr(0x0007);
 
-        /// <summary>
-        /// The window is being restored to a maximized state.
-        /// </summary>
         public const int WPF_RESTORETOMAXIMIZED = 2;
 
-        /// <summary>
-        /// Restore the window.
-        /// </summary>
         public const int SW_RESTORE = 9;
-
-        /// <summary>
-        /// Show the window, but in a minimized state.
-        /// </summary>
         public const int SW_SHOWMINIMIZED = 2;
-
-        /// <summary>
-        /// Maximize the window.
-        /// </summary>
         public const int SW_MAXIMIZE = 3;
-
-        /// <summary>
-        /// Show the window.
-        /// </summary>
         public const int SW_SHOW = 5;
-
-        /// <summary>
-        /// Minimize the window.
-        /// </summary>
         public const int SW_MINIMIZE = 6;
 
-        /// <summary>
-        /// Specifies the style setting of a window.
-        /// </summary>
         public const int GWL_STYLE = -16;
-
-        /// <summary>
-        /// Specifies the extended style setting of a window.
-        /// </summary>
         public const int GWL_EXSTYLE = -20;
 
-        /// <summary>
-        /// The window is maximized.
-        /// </summary>
         public const long WS_MAXIMIZE = 0x01000000;
-
-        /// <summary>
-        /// The window is minimized.
-        /// </summary>
         public const long WS_MINIMIZE = 0x20000000;
-
-        /// <summary>
-        /// The window is a popup window.
-        /// </summary>
         public const long WS_POPUP = 0x80000000;
-
-        /// <summary>
-        /// The window has a system menu.
-        /// </summary>
         public const long WS_SYSMENU = 0x00080000;
 
-        /// <summary>
-        /// The window is a topmost window.
-        /// </summary>
         public const long WS_EX_TOPMOST = 0x00000008;
 
-        /// <summary>
-        /// Don't change the window's size.
-        /// </summary>
         public const uint SWP_NOSIZE = 0x0001;
-
-        /// <summary>
-        /// Don't change the window's position.
-        /// </summary>
         public const uint SWP_NOMOVE = 0x0002;
-
-        /// <summary>
-        /// Don't change the window's Z-order.
-        /// </summary>
         public const uint SWP_NOZORDER = 0x0004;
-
-        /// <summary>
-        /// Don't redraw the window.
-        /// </summary>
         public const uint SWP_NOREDRAW = 0x0008;
 
-        /// <summary>
-        /// The thread is continuously active.
-        /// </summary>
         public const uint ES_CONTINUOUS = 0x80000000;
-
-        /// <summary>
-        /// The thread requires a display.
-        /// </summary>
         public const uint ES_DISPLAY_REQUIRED = 0x00000002;
 
-        /// <summary>
-        /// The default, primary monitor.
-        /// </summary>
         public const int MONITOR_DEFAULTTOPRIMARY = 1;
     }
 }
