a372e145db3b8b00590a46816c46c0b5bdf9b75f
Mike.Popoloski <Mike.Popoloski@090eb97a-982f-0410-b69a-795f42cc130f>
2008-09-17 03:16:20 +0000

    Started cleaning up and refactoring the SampleFramework.

--- trunk/samples/SampleFramework/Win32/NativeStructures.cs
+++ trunk/samples/SampleFramework/Win32/NativeStructures.cs
@@ -25,109 +25,36 @@ using System.Runtime.InteropServices;
 
 namespace SampleFramework
 {
-    /// <summary>
-    /// Represents the unmanaged RECT type.
-    /// </summary>
     [StructLayout(LayoutKind.Sequential)]
     struct NativeRectangle
     {
-        /// <summary>
-        /// The left bound of the rectangle.
-        /// </summary>
         public int left;
-
-        /// <summary>
-        /// The top bound of the rectangle.
-        /// </summary>
         public int top;
-
-        /// <summary>
-        /// The right bound of the rectangle.
-        /// </summary>
         public int right;
-
-        /// <summary>
-        /// The bottom bound of the rectangle.
-        /// </summary>
         public int bottom;
     }
 
-    /// <summary>
-    /// Represents the unmanaged MSG type.
-    /// </summary>
     [StructLayout(LayoutKind.Sequential)]
     struct NativeMessage
     {
-        /// <summary>
-        /// The window handle.
-        /// </summary>
         public IntPtr hWnd;
-
-        /// <summary>
-        /// The message ID.
-        /// </summary>
         public uint msg;
-
-        /// <summary>
-        /// The first generic parameter.
-        /// </summary>
         public IntPtr wParam;
-
-        /// <summary>
-        /// The second generic parameter.
-        /// </summary>
         public IntPtr lParam;
-
-        /// <summary>
-        /// The timing of the message.
-        /// </summary>
         public uint time;
-
-        /// <summary>
-        /// A context dependent point.
-        /// </summary>
         public Point p;
     }
 
-    /// <summary>
-    /// Represents the unmanaged WINDOWPLACEMENT type.
-    /// </summary>
     [StructLayout(LayoutKind.Sequential)]
     struct WINDOWPLACEMENT
     {
-        /// <summary>
-        /// The size of the structure, in bytes.
-        /// </summary>
         public int length;
-
-        /// <summary>
-        /// Window placement flags.
-        /// </summary>
         public int flags;
-
-        /// <summary>
-        /// The ShowWindow flag.
-        /// </summary>
         public int showCmd;
-
-        /// <summary>
-        /// The minimum point of the window.
-        /// </summary>
         public Point ptMinPosition;
-
-        /// <summary>
-        /// The maximum point of the window.
-        /// </summary>
         public Point ptMaxPosition;
-
-        /// <summary>
-        /// The restored position of the window.
-        /// </summary>
         public NativeRectangle rcNormalPosition;
 
-        /// <summary>
-        /// Gets the size of the structure, in bytes.
-        /// </summary>
         public static int Length
         {
             get { return Marshal.SizeOf(typeof(WINDOWPLACEMENT)); }
