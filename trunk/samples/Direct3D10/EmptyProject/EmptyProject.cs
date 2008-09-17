bb78186413b242dc689b6f018ae04055e46c3ca0
Mike.Popoloski <Mike.Popoloski@090eb97a-982f-0410-b69a-795f42cc130f>
2008-09-17 02:19:23 +0000

    Finished up Direct3D10 support for the SampleFramework and added an empty project that shows how to use it.

---
+++ trunk/samples/Direct3D10/EmptyProject/EmptyProject.cs
@@ -0,0 +1,102 @@
+﻿/*
+* Copyright (c) 2007-2008 SlimDX Group
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+using System.Drawing;
+using System.Windows.Forms;
+using SampleFramework;
+using SlimDX.Direct3D10;
+
+namespace EmptyProject
+{
+    /// <summary>
+    /// The top level game object. Manages the entire game.
+    /// </summary>
+    class EmptyProject : Game
+    {
+        // constants
+        const int InitialWidth = 800;
+        const int InitialHeight = 600;
+
+        /// <summary>
+        /// Gets the Direct3D device.
+        /// </summary>
+        /// <value>The Direct3D device.</value>
+        public Device Device
+        {
+            get { return GraphicsDeviceManager.Direct3D10.Device; }
+        }
+
+        /// <summary>
+        /// Gets or sets the clear color.
+        /// </summary>
+        /// <value>The clear color.</value>
+        public Color ClearColor
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EmptyProject"/> class.
+        /// </summary>
+        public EmptyProject()
+        {
+            // initialize the clear color
+            ClearColor = Color.FromArgb(0, 45, 50, 170);
+
+            // set up the window
+            Window.ClientSize = new Size(InitialWidth, InitialHeight);
+            Window.Text = "SlimDX - Empty Project Template";
+            Window.KeyDown += Window_KeyDown;
+
+            // create the Direct3D device
+            GraphicsDeviceManager.ChangeDevice(DeviceVersion.Direct3D10, true, InitialWidth, InitialHeight);
+        }
+
+        /// <summary>
+        /// Handles the KeyDown event of the Window control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.Windows.Forms.KeyEventArgs"/> instance containing the event data.</param>
+        void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            // F1 toggles between full screen and windowed mode
+            // Escape quits the application
+            if (e.KeyCode == Keys.F1)
+                GraphicsDeviceManager.ToggleFullScreen();
+            else if (e.KeyCode == Keys.Escape)
+                Exit();
+        }
+
+        /// <summary>
+        /// Called when a frame is ready to be drawn.
+        /// </summary>
+        /// <param name="gameTime">The time passed since the last frame.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            // start the scene
+            Device.ClearRenderTargetView(GraphicsDeviceManager.Direct3D10.RenderTarget, ClearColor);
+
+            // end the scene
+            GraphicsDeviceManager.Direct3D10.SwapChain.Present(0, SlimDX.DXGI.PresentFlags.None);
+        }
+    }
+}
