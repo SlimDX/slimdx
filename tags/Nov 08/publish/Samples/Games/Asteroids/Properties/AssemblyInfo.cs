a001e9911987a3f6a642a2ae8047309b26ee412a
promit.roy <promit.roy@090eb97a-982f-0410-b69a-795f42cc130f>
2008-11-14 22:15:36 +0000

    Fusing publish folder into Nov 08 release tag.

---
+++ tags/Nov 08/publish/Samples/Games/Asteroids/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Asteroids")]
+[assembly: AssemblyDescription("A sample Asteroids game for SlimDX.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SlimDX Group")]
+[assembly: AssemblyProduct("Asteroids")]
+[assembly: AssemblyCopyright("Copyright ©  2008 SlimDX Group")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c6c17156-4c2b-476d-99f5-a28b5f49d189")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
