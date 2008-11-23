8a32913553c8e4a26f8c8c10b38c040846a0a006
promit.roy <promit.roy@090eb97a-982f-0410-b69a-795f42cc130f>
2008-11-23 07:17:23 +0000

    Dropping unused references from DocScanner.

--- trunk/tools/DocScanner/ScannerConfigWindow.cs
+++ trunk/tools/DocScanner/ScannerConfigWindow.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
 using System.Xml;
-using System.Xml.Linq;
 using System.IO;
 
 namespace DocScanner
