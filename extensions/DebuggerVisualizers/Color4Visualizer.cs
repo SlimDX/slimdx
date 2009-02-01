074491e06ee163d684b404fd11c15b871ec0e6f9
Mike.Popoloski <Mike.Popoloski@090eb97a-982f-0410-b69a-795f42cc130f>
2009-02-01 21:16:07 +0000

    Added the structure for a visualizer project.

---
+++ extensions/DebuggerVisualizers/Color4Visualizer.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.DebuggerVisualizers;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace DebuggerVisualizers
+{
+    // TODO: Add the following to SomeType's definition to see this visualizer when debugging instances of SomeType:
+    // 
+    //  [DebuggerVisualizer(typeof(ColorVisualizer))]
+    //  [Serializable]
+    //  public class SomeType
+    //  {
+    //   ...
+    //  }
+    // 
+    /// <summary>
+    /// A Visualizer for Color4.  
+    /// </summary>
+    public class Color4Visualizer : DialogDebuggerVisualizer
+    {
+        protected override void Show(IDialogVisualizerService windowService, IVisualizerObjectProvider objectProvider)
+        {
+            // TODO: Get the object to display a visualizer for.
+            //       Cast the result of objectProvider.GetObject() 
+            //       to the type of the object being visualized.
+            object data = (object)objectProvider.GetObject();
+
+            // TODO: Display your view of the object.
+            //       Replace displayForm with your own custom Form or Control.
+            using (Form displayForm = new Form())
+            {
+                displayForm.Text = data.ToString();
+                windowService.ShowDialog(displayForm);
+            }
+        }
+
+        // TODO: Add the following to your testing code to test the visualizer:
+        // 
+        //    ColorVisualizer.TestShowVisualizer(new SomeType());
+        // 
+        /// <summary>
+        /// Tests the visualizer by hosting it outside of the debugger.
+        /// </summary>
+        /// <param name="objectToVisualize">The object to display in the visualizer.</param>
+        public static void TestShowVisualizer(object objectToVisualize)
+        {
+            VisualizerDevelopmentHost visualizerHost = new VisualizerDevelopmentHost(objectToVisualize, typeof(Color4Visualizer));
+            visualizerHost.ShowVisualizer();
+        }
+    }
+}
