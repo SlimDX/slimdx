5df534da45b2abda95464b4086f3748367c51989
Mike.Popoloski <Mike.Popoloski@090eb97a-982f-0410-b69a-795f42cc130f>
2009-03-25 03:20:45 +0000

    Added notification positions to capture buffers. Resolves issue 449.

--- source/directsound/CaptureBuffer.cpp
+++ source/directsound/CaptureBuffer.cpp
@@ -140,6 +140,27 @@ namespace DirectSound
 		return Unlock( stream1, stream2 );
 	}
 
+	Result CaptureBuffer::SetNotificationPositions( array<NotificationPosition>^ positions )
+	{
+		IDirectSoundNotify *pointer;
+		HRESULT hr = InternalPointer->QueryInterface( IID_IDirectSoundNotify, reinterpret_cast<void**>( &pointer ) );
+
+		if( FAILED( hr ) )
+			return RECORD_DSOUND( hr );
+
+		std::vector<DSBPOSITIONNOTIFY> notifies( positions->Length );
+		for( int i = 0; i < positions->Length; i++ )
+		{
+			notifies[i].dwOffset = positions[i].Offset;
+			notifies[i].hEventNotify = positions[i].Event->SafeWaitHandle->DangerousGetHandle().ToPointer();
+		}
+
+		hr = pointer->SetNotificationPositions( positions->Length, &notifies[0] );
+		pointer->Release();
+
+		return RECORD_DSOUND( hr );
+	}
+
 	WaveFormat^ CaptureBuffer::Format::get()
 	{
 		WaveFormat^ result;
