116c13db448501ed61305e7a4a1a2feeed4cce94
promit.roy <promit.roy@090eb97a-982f-0410-b69a-795f42cc130f>
2009-03-26 20:50:06 +0000

    Adding the skeleton of the new website.

---
+++ website/features.php
@@ -0,0 +1,16 @@
+<?php
+	$g_pageTitle = "SlimDX Features";
+	$g_pageName = "Features";
+	
+	require_once("header.php");
+?>
+
+<div id="content">
+	<div class="wrapper">    
+    </div>
+</div>
+
+<?php	
+	require_once("footer.php");
+?>
+
---
+++ website/news.php
@@ -0,0 +1,16 @@
+<?php
+	$g_pageTitle = "SlimDX News";
+	$g_pageName = "News";
+	
+	require_once("header.php");
+?>
+
+<div id="content">
+	<div class="wrapper">    
+    </div>
+</div>
+
+<?php	
+	require_once("footer.php");
+?>
+
