85b0065f06fcadf02843faef0c9f91b74b00b3dd
ryoohki <ryoohki@090eb97a-982f-0410-b69a-795f42cc130f>
2009-04-07 00:35:12 +0000

    Added full set of Color operations to native side.

--- branches/SlimMath/source/native/BatchProcessor.cpp
+++ branches/SlimMath/source/native/BatchProcessor.cpp
@@ -35,17 +35,56 @@ namespace SlimMath
 		{
 			switch(ops[i].Op)
 			{
-				case NativeOperation::MatrixIdentity:
-					StoreMatrixResult(ops[i].Result.Data, XMMatrixIdentity());
-					break;
-				case NativeOperation::MatrixMultiply:
+			case NativeOperation::ColorAdjustContrast:
+				StoreVectorResult(ops[i].Result.Data, XMColorAdjustContrast(GetVectorParameter(ops[i].Parameters[0].Data), *ops[i].Parameters[1].Data));
+				break;
+			case NativeOperation::ColorAdjustSaturation:
+				StoreVectorResult(ops[i].Result.Data, XMColorAdjustSaturation(GetVectorParameter(ops[i].Parameters[0].Data), *ops[i].Parameters[1].Data));
+				break;
+			case NativeOperation::ColorEqual:
+				*reinterpret_cast<int*>(ops[i].Result.Data) = XMColorEqual(GetVectorParameter(ops[i].Parameters[0].Data), GetVectorParameter(ops[i].Parameters[1].Data));
+				break;
+			case NativeOperation::ColorGreater:
+				*reinterpret_cast<int*>(ops[i].Result.Data) = XMColorGreater(GetVectorParameter(ops[i].Parameters[0].Data), GetVectorParameter(ops[i].Parameters[1].Data));
+				break;
+			case NativeOperation::ColorGreaterOrEqual:
+				*reinterpret_cast<int*>(ops[i].Result.Data) = XMColorGreaterOrEqual(GetVectorParameter(ops[i].Parameters[0].Data), GetVectorParameter(ops[i].Parameters[1].Data));
+				break;
+			case NativeOperation::ColorIsInfinite:
+				*reinterpret_cast<int*>(ops[i].Result.Data) = XMColorIsInfinite(GetVectorParameter(ops[i].Parameters[0].Data));
+				break;
+			case NativeOperation::ColorIsNaN:
+				*reinterpret_cast<int*>(ops[i].Result.Data) = XMColorIsNaN(GetVectorParameter(ops[i].Parameters[0].Data));
+				break;
+			case NativeOperation::ColorLess:
+				*reinterpret_cast<int*>(ops[i].Result.Data) = XMColorLess(GetVectorParameter(ops[i].Parameters[0].Data), GetVectorParameter(ops[i].Parameters[1].Data));
+				break;
+			case NativeOperation::ColorLessOrEqual:
+				*reinterpret_cast<int*>(ops[i].Result.Data) = XMColorLessOrEqual(GetVectorParameter(ops[i].Parameters[0].Data), GetVectorParameter(ops[i].Parameters[1].Data));
+				break;
+			case NativeOperation::ColorModulate:
+				StoreVectorResult(ops[i].Result.Data, XMColorModulate(GetVectorParameter(ops[i].Parameters[0].Data), GetVectorParameter(ops[i].Parameters[1].Data)));
+				break;
+			/*case NativeOperation::ColorNearEqual:
+				*((int*)ops[i].Result.Data) = XMColorNearEqual(GetVectorParameter(ops[i].Parameters[0].Data), GetVectorParameter(ops[i].Parameters[1].Data), GetVectorParameter(ops[i].Parameters[2].Data));
+				break;*/
+			case NativeOperation::ColorNegative:
+				StoreVectorResult(ops[i].Result.Data, XMColorNegative(GetVectorParameter(ops[i].Parameters[0].Data)));
+				break;
+			case NativeOperation::ColorNotEqual:
+				*reinterpret_cast<int*>(ops[i].Result.Data) = XMColorNotEqual(GetVectorParameter(ops[i].Parameters[0].Data), GetVectorParameter(ops[i].Parameters[1].Data));
+				break;
+			case NativeOperation::MatrixIdentity:
+				StoreMatrixResult(ops[i].Result.Data, XMMatrixIdentity());
+				break;
+			case NativeOperation::MatrixMultiply:
 				{
 					XMMATRIX p1 = GetMatrixParameter(ops[i].Parameters[0].Data);
 					XMMATRIX p2 = GetMatrixParameter(ops[i].Parameters[1].Data);
 					StoreMatrixResult(ops[i].Result.Data, XMMatrixMultiply(p1, p2));
 					break;
 				}
-				case NativeOperation::MatrixInverse:
+			case NativeOperation::MatrixInverse:
 				{
 					XMMATRIX p1 = GetMatrixParameter(ops[i].Parameters[0].Data);
 					XMVECTOR det;
@@ -53,14 +92,14 @@ namespace SlimMath
 					XMStoreFloat(ops[i].Result.Data + 16, det);
 					break;
 				}
-				case NativeOperation::MatrixTranslationFromVector:
-					StoreMatrixResult(ops[i].Result.Data, XMMatrixTranslationFromVector(GetVectorParameter(ops[i].Parameters[0].Data)));
-					break;
-				case NativeOperation::Vector4Transform:
-					StoreVectorResult(ops[i].Result.Data, XMVector4Transform(GetVectorParameter(ops[i].Parameters[0].Data), GetMatrixParameter(ops[i].Parameters[1].Data)));
-					break;
-				case NativeOperation::Vector4TransformStream:
-					XMVector4TransformStream(reinterpret_cast<XMFLOAT4*>(ops[i].Result.Data), 16, reinterpret_cast<XMFLOAT4*>(ops[i].Parameters[0].Data), 16, reinterpret_cast<int>(ops[i].Parameters[2].Data), GetMatrixParameter(ops[i].Parameters[1].Data));
+			case NativeOperation::MatrixTranslationFromVector:
+				StoreMatrixResult(ops[i].Result.Data, XMMatrixTranslationFromVector(GetVectorParameter(ops[i].Parameters[0].Data)));
+				break;
+			case NativeOperation::Vector4Transform:
+				StoreVectorResult(ops[i].Result.Data, XMVector4Transform(GetVectorParameter(ops[i].Parameters[0].Data), GetMatrixParameter(ops[i].Parameters[1].Data)));
+				break;
+			case NativeOperation::Vector4TransformStream:
+				XMVector4TransformStream(reinterpret_cast<XMFLOAT4*>(ops[i].Result.Data), 16, reinterpret_cast<XMFLOAT4*>(ops[i].Parameters[0].Data), 16, reinterpret_cast<int>(ops[i].Parameters[2].Data), GetMatrixParameter(ops[i].Parameters[1].Data));
 			}
 		}
 	}
