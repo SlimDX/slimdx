402a1d6f94136af41af2d65882f07fe0169af77f
josh.petrie <josh.petrie@090eb97a-982f-0410-b69a-795f42cc130f>
2009-06-28 04:57:39 +0000

    Turning on unit tests that are not constrained by the interface changes.

--- tests/SlimDX.Tests/source/DXGI.Adapter.Tests.cpp
+++ tests/SlimDX.Tests/source/DXGI.Adapter.Tests.cpp
@@ -1,128 +1,128 @@
 #include "stdafx.h"
-///*
-//* Copyright (c) 2007-2009 SlimDX Group
-//* 
-//* Permission is hereby granted, free of charge, to any person obtaining a copy
-//* of this software and associated documentation files (the "Software"), to deal
-//* in the Software without restriction, including without limitation the rights
-//* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//* copies of the Software, and to permit persons to whom the Software is
-//* furnished to do so, subject to the following conditions:
-//* 
-//* The above copyright notice and this permission notice shall be included in
-//* all copies or substantial portions of the Software.
-//* 
-//* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//* THE SOFTWARE.
-//*/
-//
-//#include "IDXGIAdapterMock.h"
-//
-//#include "Asserts.h"
-//#include "ScopedThrowOnError.h"
-//
-//#include "IDXGIOutputMock.h"
-//
-//using namespace testing;
-//using namespace System;
-//using namespace SlimDX;
-//using namespace SlimDX::DXGI;
-//
-//TEST( DXGI_AdapterTests, GetDescription )
-//{
-//	IDXGIAdapterMock mockAdapter;
-//	Adapter^ adapter = Adapter::FromPointer( System::IntPtr( &mockAdapter ) );
-//	
-//	LUID expectedLuid;
-//	expectedLuid.HighPart = 0xAABBCCDD;
-//	expectedLuid.LowPart = 0x00112233;
-//
-//	DXGI_ADAPTER_DESC expectedDescription;
-//	StringCchPrintf( expectedDescription.Description, 128, L"Fake Adapter" );
-//	expectedDescription.VendorId = 1;
-//	expectedDescription.DeviceId = 2;
-//	expectedDescription.SubSysId = 3;
-//	expectedDescription.Revision = 4;
-//	expectedDescription.DedicatedVideoMemory = 1000;
-//	expectedDescription.DedicatedSystemMemory = 2000;
-//	expectedDescription.SharedSystemMemory = 3000;
-//	expectedDescription.AdapterLuid = expectedLuid;
-//
-//	EXPECT_CALL( mockAdapter, GetDesc( _ ) )
-//		.WillOnce( DoAll( SetArgumentPointee<0>( expectedDescription ), Return( S_OK ) ) );
-//	
-//	AdapterDescription description = adapter->Description;
-//	ASSERT_TRUE( Result::Last.IsSuccess );
-//
-//	ASSERT_TRUE( L"Fake Adapter" == description.Description );
-//	ASSERT_EQ( 1, description.VendorId );
-//	ASSERT_EQ( 2, description.DeviceId );
-//	ASSERT_EQ( 3, description.SubsystemId );
-//	ASSERT_EQ( 4, description.Revision );
-//	ASSERT_EQ( 1000, description.DedicatedVideoMemory );
-//	ASSERT_EQ( 2000, description.DedicatedSystemMemory );
-//	ASSERT_EQ( 3000, description.SharedSystemMemory );
-//	ASSERT_EQ( 0xAABBCCDD00112233, description.Luid );
-//	
-//	delete adapter;
-//}
-//
-//TEST( DXGI_AdapterTests, GetDescriptionThrows )
-//{
-//	IDXGIAdapterMock mockAdapter;
-//	Adapter^ adapter = Adapter::FromPointer( System::IntPtr( &mockAdapter ) );
-//	
-//	EXPECT_CALL( mockAdapter, GetDesc( _ ) )
-//		.WillRepeatedly( Return( E_FAIL ) );
-//	
-//	ASSERT_MANAGED_THROW( adapter->Description, DXGIException );
-//	ASSERT_TRUE( Result::Last.IsFailure );
-//
-//	delete adapter;
-//}
-//
-//TEST( DXGI_AdapterTests, GetOutputCountCanReturnZero )
-//{
-//	IDXGIAdapterMock mockAdapter;
-//	Adapter^ adapter = Adapter::FromPointer( System::IntPtr( &mockAdapter ) );
-//	
-//	EXPECT_CALL( mockAdapter, EnumOutputs( _, _ ) )
-//		.WillOnce( Return( DXGI_ERROR_NOT_FOUND ) );
-//	
-//	int count = adapter->GetOutputCount();
-//	ASSERT_EQ( 0, count );
-//
-//	delete adapter;
-//}
-//
-//TEST( DXGI_AdapterTests, GetOutputCountCanReturnNonZero )
-//{
-//	IDXGIAdapterMock mockAdapter;
-//	IDXGIOutputMock mockOutput0;
-//	IDXGIOutputMock mockOutput1;
-//	IDXGIOutputMock mockOutput2;
-//	Adapter^ adapter = Adapter::FromPointer( System::IntPtr( &mockAdapter ) );
-//	
-//	EXPECT_CALL( mockAdapter, EnumOutputs( 0, _ ) )
-//		.WillOnce( DoAll( SetArgumentPointee<1>( &mockOutput0 ), Return( S_OK ) ) );
-//	EXPECT_CALL( mockAdapter, EnumOutputs( 1, _ ) )
-//		.WillOnce( DoAll( SetArgumentPointee<1>( &mockOutput1 ), Return( S_OK ) ) );
-//	EXPECT_CALL( mockAdapter, EnumOutputs( 2, _ ) )
-//		.WillOnce( DoAll( SetArgumentPointee<1>( &mockOutput2 ), Return( S_OK ) ) );
-//	EXPECT_CALL( mockAdapter, EnumOutputs( 3, _ ) )
-//		.WillOnce( Return( DXGI_ERROR_NOT_FOUND ) );
-//	
-//	int count = adapter->GetOutputCount();
-//	ASSERT_EQ( 3, count );
-//
-//	delete adapter;
-//}
-//
+/*
+* Copyright (c) 2007-2009 SlimDX Group
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+
+#include "IDXGIAdapterMock.h"
+
+#include "Asserts.h"
+#include "ScopedThrowOnError.h"
+
+#include "IDXGIOutputMock.h"
+
+using namespace testing;
+using namespace System;
+using namespace SlimDX;
+using namespace SlimDX::DXGI;
+
+TEST( DXGI_AdapterTests, GetDescription )
+{
+	IDXGIAdapterMock mockAdapter;
+	Adapter^ adapter = Adapter::FromPointer( System::IntPtr( &mockAdapter ) );
+	
+	LUID expectedLuid;
+	expectedLuid.HighPart = 0xAABBCCDD;
+	expectedLuid.LowPart = 0x00112233;
+
+	DXGI_ADAPTER_DESC expectedDescription;
+	StringCchPrintf( expectedDescription.Description, 128, L"Fake Adapter" );
+	expectedDescription.VendorId = 1;
+	expectedDescription.DeviceId = 2;
+	expectedDescription.SubSysId = 3;
+	expectedDescription.Revision = 4;
+	expectedDescription.DedicatedVideoMemory = 1000;
+	expectedDescription.DedicatedSystemMemory = 2000;
+	expectedDescription.SharedSystemMemory = 3000;
+	expectedDescription.AdapterLuid = expectedLuid;
+
+	EXPECT_CALL( mockAdapter, GetDesc( _ ) )
+		.WillOnce( DoAll( SetArgumentPointee<0>( expectedDescription ), Return( S_OK ) ) );
+	
+	AdapterDescription description = adapter->Description;
+	ASSERT_TRUE( Result::Last.IsSuccess );
+
+	ASSERT_TRUE( L"Fake Adapter" == description.Description );
+	ASSERT_EQ( 1, description.VendorId );
+	ASSERT_EQ( 2, description.DeviceId );
+	ASSERT_EQ( 3, description.SubsystemId );
+	ASSERT_EQ( 4, description.Revision );
+	ASSERT_EQ( 1000, description.DedicatedVideoMemory );
+	ASSERT_EQ( 2000, description.DedicatedSystemMemory );
+	ASSERT_EQ( 3000, description.SharedSystemMemory );
+	ASSERT_EQ( 0xAABBCCDD00112233, description.Luid );
+	
+	delete adapter;
+}
+
+TEST( DXGI_AdapterTests, GetDescriptionThrows )
+{
+	IDXGIAdapterMock mockAdapter;
+	Adapter^ adapter = Adapter::FromPointer( System::IntPtr( &mockAdapter ) );
+	
+	EXPECT_CALL( mockAdapter, GetDesc( _ ) )
+		.WillRepeatedly( Return( E_FAIL ) );
+	
+	ASSERT_MANAGED_THROW( adapter->Description, DXGIException );
+	ASSERT_TRUE( Result::Last.IsFailure );
+
+	delete adapter;
+}
+
+TEST( DXGI_AdapterTests, GetOutputCountCanReturnZero )
+{
+	IDXGIAdapterMock mockAdapter;
+	Adapter^ adapter = Adapter::FromPointer( System::IntPtr( &mockAdapter ) );
+	
+	EXPECT_CALL( mockAdapter, EnumOutputs( _, _ ) )
+		.WillOnce( Return( DXGI_ERROR_NOT_FOUND ) );
+	
+	int count = adapter->GetOutputCount();
+	ASSERT_EQ( 0, count );
+
+	delete adapter;
+}
+
+TEST( DXGI_AdapterTests, GetOutputCountCanReturnNonZero )
+{
+	IDXGIAdapterMock mockAdapter;
+	IDXGIOutputMock mockOutput0;
+	IDXGIOutputMock mockOutput1;
+	IDXGIOutputMock mockOutput2;
+	Adapter^ adapter = Adapter::FromPointer( System::IntPtr( &mockAdapter ) );
+	
+	EXPECT_CALL( mockAdapter, EnumOutputs( 0, _ ) )
+		.WillOnce( DoAll( SetArgumentPointee<1>( &mockOutput0 ), Return( S_OK ) ) );
+	EXPECT_CALL( mockAdapter, EnumOutputs( 1, _ ) )
+		.WillOnce( DoAll( SetArgumentPointee<1>( &mockOutput1 ), Return( S_OK ) ) );
+	EXPECT_CALL( mockAdapter, EnumOutputs( 2, _ ) )
+		.WillOnce( DoAll( SetArgumentPointee<1>( &mockOutput2 ), Return( S_OK ) ) );
+	EXPECT_CALL( mockAdapter, EnumOutputs( 3, _ ) )
+		.WillOnce( Return( DXGI_ERROR_NOT_FOUND ) );
+	
+	int count = adapter->GetOutputCount();
+	ASSERT_EQ( 3, count );
+
+	delete adapter;
+}
+
 //TEST( DXGI_AdapterTests, GetOutput )
 //{
 //	IDXGIAdapterMock mockAdapter;
--- tests/SlimDX.Tests/source/DXGI.Factory.Tests.cpp
+++ tests/SlimDX.Tests/source/DXGI.Factory.Tests.cpp
@@ -1,75 +1,74 @@
 #include "stdafx.h"
-///*
-//* Copyright (c) 2007-2009 SlimDX Group
-//* 
-//* Permission is hereby granted, free of charge, to any person obtaining a copy
-//* of this software and associated documentation files (the "Software"), to deal
-//* in the Software without restriction, including without limitation the rights
-//* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//* copies of the Software, and to permit persons to whom the Software is
-//* furnished to do so, subject to the following conditions:
-//* 
-//* The above copyright notice and this permission notice shall be included in
-//* all copies or substantial portions of the Software.
-//* 
-//* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//* THE SOFTWARE.
-//*/
-//
-//#include "IDXGIFactoryMock.h"
-//
-//#include "Asserts.h"
-//#include "ScopedThrowOnError.h"
-//
-//#include "IDXGIAdapterMock.h"
-//
-//using namespace testing;
-//using namespace System;
-//using namespace SlimDX::DXGI;
-//
-//TEST( DXGI_FactoryTests, GetAdapterCountCanReturnZero )
-//{
-//	IDXGIFactoryMock mockFactory;
-//	Factory^ factory = Factory::FromPointer( System::IntPtr( &mockFactory ) );
-//	
-//	EXPECT_CALL( mockFactory, EnumAdapters( _, _ ) )
-//		.WillOnce( Return( DXGI_ERROR_NOT_FOUND ) );
-//	
-//	int count = factory->GetAdapterCount();
-//	ASSERT_EQ( 0, count );
-//
-//	delete factory;
-//}
-//
-//TEST( DXGI_FactoryTests, GetAdapterCountCanReturnNonZero )
-//{
-//	IDXGIFactoryMock mockFactory;
-//	IDXGIAdapterMock mockAdapter0;
-//	IDXGIAdapterMock mockAdapter1;
-//	IDXGIAdapterMock mockAdapter2;
-//	Factory^ factory = Factory::FromPointer( System::IntPtr( &mockFactory ) );
-//	
-//	EXPECT_CALL( mockFactory, EnumAdapters( 0, _ ) )
-//		.WillOnce( DoAll( SetArgumentPointee<1>( &mockAdapter0 ), Return( S_OK ) ) );
-//	EXPECT_CALL( mockFactory, EnumAdapters( 1, _ ) )
-//		.WillOnce( DoAll( SetArgumentPointee<1>( &mockAdapter1 ), Return( S_OK ) ) );
-//	EXPECT_CALL( mockFactory, EnumAdapters( 2, _ ) )
-//		.WillOnce( DoAll( SetArgumentPointee<1>( &mockAdapter2 ), Return( S_OK ) ) );
-//	EXPECT_CALL( mockFactory, EnumAdapters( 3, _ ) )
-//		.WillOnce( Return( DXGI_ERROR_NOT_FOUND ) );
-//	
-//	int count = factory->GetAdapterCount();
-//	ASSERT_EQ( 3, count );
-//
-//	delete factory;
-//}
-//
-//
+/*
+* Copyright (c) 2007-2009 SlimDX Group
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+
+#include "IDXGIFactoryMock.h"
+
+#include "Asserts.h"
+#include "ScopedThrowOnError.h"
+
+#include "IDXGIAdapterMock.h"
+
+using namespace testing;
+using namespace System;
+using namespace SlimDX::DXGI;
+
+TEST( DXGI_FactoryTests, GetAdapterCountCanReturnZero )
+{
+	IDXGIFactoryMock mockFactory;
+	Factory^ factory = Factory::FromPointer( System::IntPtr( &mockFactory ) );
+	
+	EXPECT_CALL( mockFactory, EnumAdapters( _, _ ) )
+		.WillOnce( Return( DXGI_ERROR_NOT_FOUND ) );
+	
+	int count = factory->GetAdapterCount();
+	ASSERT_EQ( 0, count );
+
+	delete factory;
+}
+
+TEST( DXGI_FactoryTests, GetAdapterCountCanReturnNonZero )
+{
+	IDXGIFactoryMock mockFactory;
+	IDXGIAdapterMock mockAdapter0;
+	IDXGIAdapterMock mockAdapter1;
+	IDXGIAdapterMock mockAdapter2;
+	Factory^ factory = Factory::FromPointer( System::IntPtr( &mockFactory ) );
+	
+	EXPECT_CALL( mockFactory, EnumAdapters( 0, _ ) )
+		.WillOnce( DoAll( SetArgumentPointee<1>( &mockAdapter0 ), Return( S_OK ) ) );
+	EXPECT_CALL( mockFactory, EnumAdapters( 1, _ ) )
+		.WillOnce( DoAll( SetArgumentPointee<1>( &mockAdapter1 ), Return( S_OK ) ) );
+	EXPECT_CALL( mockFactory, EnumAdapters( 2, _ ) )
+		.WillOnce( DoAll( SetArgumentPointee<1>( &mockAdapter2 ), Return( S_OK ) ) );
+	EXPECT_CALL( mockFactory, EnumAdapters( 3, _ ) )
+		.WillOnce( Return( DXGI_ERROR_NOT_FOUND ) );
+	
+	int count = factory->GetAdapterCount();
+	ASSERT_EQ( 3, count );
+
+	delete factory;
+}
+
 //TEST( DXGI_FactoryTests, GetAdapter )
 //{
 //	IDXGIFactoryMock mockFactory;
