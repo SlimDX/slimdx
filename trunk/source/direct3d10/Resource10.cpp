1c3ca179d1eeafd8c45b45c299db6e360acf7cb2
josh.petrie <josh.petrie@090eb97a-982f-0410-b69a-795f42cc130f>
2009-07-02 01:55:13 +0000

    Added subresource calculation methods to 10 and 11. Also tests for 10. Resolves issue #510.

--- trunk/source/direct3d10/Resource10.cpp
+++ trunk/source/direct3d10/Resource10.cpp
@@ -98,6 +98,11 @@ namespace Direct3D10
 		return static_cast<ResourceDimension>( type );
 	}
 
+	int Resource::CalculateSubresourceIndex( int mipSlice, int arraySlice, int mipLevels )
+	{
+		return D3D10CalcSubresource( static_cast<UINT>( mipSlice ), static_cast<UINT>( arraySlice ), static_cast<UINT>( mipLevels ) );
+	}
+
 	int Resource::GetMipSize( int mipSlice, int baseSliceSize )
 	{
 		float size = static_cast<float>( baseSliceSize );
