b7bfbced0ebccf168b3e2d22657c7b5c4cc2b829
mike.popoloski <mike.popoloski@090eb97a-982f-0410-b69a-795f42cc130f>
2009-10-29 16:43:53 +0000

    Added missing Flags field to D3D11 DepthStencilViewDescription. Resolves issue 559.

--- trunk/source/direct3d11/DepthStencilViewDescription11.cpp
+++ trunk/source/direct3d11/DepthStencilViewDescription11.cpp
@@ -30,35 +30,42 @@ namespace SlimDX
 namespace Direct3D11
 {
 	DepthStencilViewDescription::DepthStencilViewDescription( const D3D11_DEPTH_STENCIL_VIEW_DESC& native )
-	: m_MipSlice( 0 ), m_ArraySize( 0 ), m_FirstArraySlice( 0 )
 	{
-		m_Format = static_cast<DXGI::Format>( native.Format );
-		m_ViewDimension = static_cast<DepthStencilViewDimension>( native.ViewDimension );
-		switch( m_ViewDimension )
+		Format = static_cast<DXGI::Format>( native.Format );
+		Dimension = static_cast<DepthStencilViewDimension>( native.ViewDimension );
+		Flags = static_cast<DepthStencilViewFlags>( native.Flags );
+
+		switch( Dimension )
 		{
 			case DepthStencilViewDimension::Texture1D:
-				m_MipSlice = native.Texture1D.MipSlice;
+				MipSlice = native.Texture1D.MipSlice;
 				break;
+
 			case DepthStencilViewDimension::Texture1DArray:
-				m_ArraySize = native.Texture1DArray.ArraySize;
-				m_FirstArraySlice = native.Texture1DArray.FirstArraySlice;
-				m_MipSlice = native.Texture1DArray.MipSlice;
+				ArraySize = native.Texture1DArray.ArraySize;
+				FirstArraySlice = native.Texture1DArray.FirstArraySlice;
+				MipSlice = native.Texture1DArray.MipSlice;
 				break;
+
 			case DepthStencilViewDimension::Texture2D:
-				m_MipSlice = native.Texture2D.MipSlice;
+				MipSlice = native.Texture2D.MipSlice;
 				break;
+
 			case DepthStencilViewDimension::Texture2DArray:
-				m_ArraySize = native.Texture2DArray.ArraySize;
-				m_FirstArraySlice = native.Texture2DArray.FirstArraySlice;
-				m_MipSlice = native.Texture2DArray.MipSlice;
+				ArraySize = native.Texture2DArray.ArraySize;
+				FirstArraySlice = native.Texture2DArray.FirstArraySlice;
+				MipSlice = native.Texture2DArray.MipSlice;
 				break;
+
 			case DepthStencilViewDimension::Texture2DMultisampled:
 				// Nothing to do here.
 				break;
+
 			case DepthStencilViewDimension::Texture2DMultisampledArray:
-				m_ArraySize = native.Texture2DMSArray.ArraySize;
-				m_FirstArraySlice = native.Texture2DMSArray.FirstArraySlice;
+				ArraySize = native.Texture2DMSArray.ArraySize;
+				FirstArraySlice = native.Texture2DMSArray.FirstArraySlice;
 				break;
+
 			default:
 				break;
 		}
@@ -67,88 +74,46 @@ namespace Direct3D11
 	D3D11_DEPTH_STENCIL_VIEW_DESC DepthStencilViewDescription::CreateNativeVersion()
 	{
 		D3D11_DEPTH_STENCIL_VIEW_DESC native;
-		native.Format = static_cast<DXGI_FORMAT>( m_Format );
-		native.ViewDimension = static_cast<D3D11_DSV_DIMENSION>( m_ViewDimension );
-		switch( m_ViewDimension )
+		native.Format = static_cast<DXGI_FORMAT>( Format );
+		native.ViewDimension = static_cast<D3D11_DSV_DIMENSION>( Dimension );
+		native.Flags = static_cast<UINT>( Flags );
+
+		switch( Dimension )
 		{
 			case DepthStencilViewDimension::Texture1D:
-				native.Texture1D.MipSlice = m_MipSlice;
+				native.Texture1D.MipSlice = MipSlice;
 				break;
+
 			case DepthStencilViewDimension::Texture1DArray:
-				native.Texture1DArray.ArraySize = m_ArraySize;
-				native.Texture1DArray.FirstArraySlice = m_FirstArraySlice;
-				native.Texture1DArray.MipSlice = m_MipSlice;
+				native.Texture1DArray.ArraySize = ArraySize;
+				native.Texture1DArray.FirstArraySlice = FirstArraySlice;
+				native.Texture1DArray.MipSlice = MipSlice;
 				break;
+
 			case DepthStencilViewDimension::Texture2D:
-				native.Texture2D.MipSlice = m_MipSlice;
+				native.Texture2D.MipSlice = MipSlice;
 				break;
+
 			case DepthStencilViewDimension::Texture2DArray:
-				native.Texture2DArray.ArraySize = m_ArraySize;
-				native.Texture2DArray.FirstArraySlice = m_FirstArraySlice;
-				native.Texture2DArray.MipSlice = m_MipSlice;
+				native.Texture2DArray.ArraySize = ArraySize;
+				native.Texture2DArray.FirstArraySlice = FirstArraySlice;
+				native.Texture2DArray.MipSlice = MipSlice;
 				break;
+
 			case DepthStencilViewDimension::Texture2DMultisampled:
 				// Nothing to do here.
 				break;
+
 			case DepthStencilViewDimension::Texture2DMultisampledArray:
-				native.Texture2DMSArray.ArraySize = m_ArraySize;
-				native.Texture2DMSArray.FirstArraySlice = m_FirstArraySlice;
+				native.Texture2DMSArray.ArraySize = ArraySize;
+				native.Texture2DMSArray.FirstArraySlice = FirstArraySlice;
 				break;
+
 			default:
 				break;
 		}
 		
 		return native;
-	}
-
-	DXGI::Format DepthStencilViewDescription::Format::get()
-	{
-		return m_Format;
-	}
-
-	void DepthStencilViewDescription::Format::set( DXGI::Format value )
-	{
-		m_Format = value;
-	}
-	
-	DepthStencilViewDimension DepthStencilViewDescription::Dimension::get()
-	{
-		return m_ViewDimension;
-	}
-
-	void DepthStencilViewDescription::Dimension::set( DepthStencilViewDimension value )
-	{
-		m_ViewDimension = value;
-	}
-
-	int DepthStencilViewDescription::MipSlice::get()
-	{
-		return m_MipSlice;
-	}
-
-	void DepthStencilViewDescription::MipSlice::set( int value )
-	{
-		m_MipSlice = value;
-	}
-	
-	int DepthStencilViewDescription::ArraySize::get()
-	{
-		return m_ArraySize;
-	}
-
-	void DepthStencilViewDescription::ArraySize::set( int value )
-	{
-		m_ArraySize = value;
-	}
-	
-	int DepthStencilViewDescription::FirstArraySlice::get()
-	{
-		return m_FirstArraySlice;
-	}
-
-	void DepthStencilViewDescription::FirstArraySlice::set( int value )
-	{
-		m_FirstArraySlice = value;
 	}
 
 	bool DepthStencilViewDescription::operator == ( DepthStencilViewDescription left, DepthStencilViewDescription right )
@@ -163,8 +128,8 @@ namespace Direct3D11
 
 	int DepthStencilViewDescription::GetHashCode()
 	{
-		return m_Format.GetHashCode() + m_ViewDimension.GetHashCode() + m_MipSlice.GetHashCode()
-			 + m_FirstArraySlice.GetHashCode() + m_ArraySize.GetHashCode();
+		return Format.GetHashCode() + Dimension.GetHashCode() + MipSlice.GetHashCode()
+			 + FirstArraySlice.GetHashCode() + ArraySize.GetHashCode() + Flags.GetHashCode();
 	}
 
 	bool DepthStencilViewDescription::Equals( Object^ value )
@@ -180,14 +145,14 @@ namespace Direct3D11
 
 	bool DepthStencilViewDescription::Equals( DepthStencilViewDescription value )
 	{
-		return ( m_Format == value.m_Format && m_ViewDimension == value.m_ViewDimension && m_MipSlice == value.m_MipSlice
-			 && m_FirstArraySlice == value.m_FirstArraySlice && m_ArraySize == value.m_ArraySize );
+		return ( Format == value.Format && Dimension == value.Dimension && MipSlice == value.MipSlice
+			 && FirstArraySlice == value.FirstArraySlice && ArraySize == value.ArraySize && Flags == value.Flags );
 	}
 
 	bool DepthStencilViewDescription::Equals( DepthStencilViewDescription% value1, DepthStencilViewDescription% value2 )
 	{
-		return ( value1.m_Format == value2.m_Format && value1.m_ViewDimension == value2.m_ViewDimension && value1.m_MipSlice == value2.m_MipSlice
-			 && value1.m_FirstArraySlice == value2.m_FirstArraySlice && value1.m_ArraySize == value2.m_ArraySize );
+		return ( value1.Format == value2.Format && value1.Dimension == value2.Dimension && value1.MipSlice == value2.MipSlice
+			 && value1.FirstArraySlice == value2.FirstArraySlice && value1.ArraySize == value2.ArraySize && value1.Flags == value2.Flags );
 	}
 }
 }
