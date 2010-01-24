0d69ce7e39416fd4ee4e71693a6b2814b0c03a8f
josh.petrie <josh.petrie@090eb97a-982f-0410-b69a-795f42cc130f>
2010-01-24 19:34:18 +0000

    Updating copyright strings in the DLL, test and samples directories.

--- samples/SampleFramework/Rendering/DynamicPrimitiveBuffer10.cs
+++ samples/SampleFramework/Rendering/DynamicPrimitiveBuffer10.cs
@@ -1,5 +1,5 @@
 ﻿/*
-* Copyright (c) 2007-2009 SlimDX Group
+* Copyright (c) 2007-2010 SlimDX Group
 * 
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
