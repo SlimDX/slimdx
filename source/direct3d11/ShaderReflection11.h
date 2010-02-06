53064ffb333a8258dd8161ab56577212cc634d1f
josh.petrie <josh.petrie@090eb97a-982f-0410-b69a-795f42cc130f>
2010-02-06 18:46:29 +0000

    Added ShaderReflection for 11.

---
+++ source/direct3d11/ShaderReflection11.h
@@ -0,0 +1,88 @@
+/*
+* Copyright (c) 2007-2010 SlimDX Group
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+#pragma once
+
+#include "../ComObject.h"
+
+namespace SlimDX
+{
+	namespace Direct3D11
+	{
+		ref class ShaderBytecode;
+
+		public ref class ShaderReflection : ComObject
+		{
+			COMOBJECT(ID3D11ShaderReflection, ShaderReflection);
+			
+		public:
+			ShaderReflection( ShaderBytecode^ byteCode );
+			
+			/// <summary>
+			/// Gets the number of bitwise instructions.
+			/// </summary>
+			property int BitwiseInstructionCount 
+			{
+				int get();
+			}
+			
+			/// <summary>
+			/// Gets the number of conversion instructions.
+			/// </summary>
+			property int ConversionInstructionCount
+			{
+				int get();
+			}
+			
+			/// <summary>
+			/// Gets the number of move instructions.
+			/// </summary>
+			property int MoveInstructionCount
+			{
+				int get();
+			}
+			
+			/// <summary>
+			/// Gets the number of conditional move instructions.
+			/// </summary>
+			property int ConditionalMoveInstructionCount
+			{
+				int get();
+			}
+			
+			/// <summary>
+			/// Gets the number of interface slots.
+			/// </summary>
+			property int InterfaceSlotCount
+			{
+				int get();
+			}
+			
+			/// <summary>
+			/// Gets a value indicating whether the shader is a sample frequency shader.
+			/// </summary>
+			property bool IsSampleFrequencyShader
+			{
+				bool get();
+			}
+		};
+	}
+}
