83f963ff925ab3865c45862fcc6c4702ecdd5171
legalize@xmission.com <legalize@xmission.com@090eb97a-982f-0410-b69a-795f42cc130f>
2010-02-26 19:22:50 +0000

    DirectWrite : Refactored : Extract Function

--- trunk/tests/SlimDX.Tests/source/DirectWrite.TextLayout.Tests.cpp
+++ trunk/tests/SlimDX.Tests/source/DirectWrite.TextLayout.Tests.cpp
@@ -77,10 +77,8 @@ TEST(TextLayoutTests, DetermineMinWidth)
 	ASSERT_EQ( 14.5f, layout->DetermineMinWidth() );
 }
 
-TEST(TextLayoutTests, HitTestPoint)
+static DWRITE_HIT_TEST_METRICS ExpectedHitTestMetrics()
 {
-	IDWriteTextLayoutMock mockLayout;
-	TextLayout ^layout = TextLayout::FromPointer(System::IntPtr(&mockLayout));
 	DWRITE_HIT_TEST_METRICS expectedMetrics;
 	expectedMetrics.textPosition = 0;
 	expectedMetrics.length = 1;
@@ -91,11 +89,32 @@ TEST(TextLayoutTests, HitTestPoint)
 	expectedMetrics.bidiLevel = 23;
 	expectedMetrics.isText = TRUE;
 	expectedMetrics.isTrimmed = TRUE;
+	return expectedMetrics;
+}
+
+static void AssertHitTestMetricsMatchExpected(HitTestMetrics% actual)
+{
+	DWRITE_HIT_TEST_METRICS expected = ExpectedHitTestMetrics();
+	ASSERT_EQ( expected.textPosition, actual.TextPosition );
+	ASSERT_EQ( expected.length, actual.Length );
+	ASSERT_EQ( expected.left, actual.Left );
+	ASSERT_EQ( expected.top, actual.Top );
+	ASSERT_EQ( expected.width, actual.Width );
+	ASSERT_EQ( expected.height, actual.Height );
+	ASSERT_EQ( expected.bidiLevel, actual.BidiLevel );
+	ASSERT_EQ( expected.isText == TRUE, actual.IsText );
+	ASSERT_EQ( expected.isTrimmed == TRUE, actual.IsTrimmed );
+}
+
+TEST(TextLayoutTests, HitTestPoint)
+{
+	IDWriteTextLayoutMock mockLayout;
+	TextLayout ^layout = TextLayout::FromPointer(System::IntPtr(&mockLayout));
 	EXPECT_CALL(mockLayout, HitTestPoint(10.0f, 12.0f, NotNull(), NotNull(), NotNull()))
 		.Times(1)
 		.WillOnce(DoAll(SetArgumentPointee<2>(false),
 						SetArgumentPointee<3>(false),
-						SetArgumentPointee<4>(expectedMetrics),
+						SetArgumentPointee<4>(ExpectedHitTestMetrics()),
 						Return(S_OK)));
 	bool isTrailingHit = true;
 	bool isInside = true;
@@ -103,49 +122,23 @@ TEST(TextLayoutTests, HitTestPoint)
 	ASSERT_TRUE( Result::Last.IsSuccess );
 	ASSERT_FALSE( isTrailingHit );
 	ASSERT_FALSE( isInside );
-	ASSERT_EQ( 0, metrics.TextPosition );
-	ASSERT_EQ( 1, metrics.Length );
-	ASSERT_EQ( 0.15f, metrics.Left );
-	ASSERT_EQ( 0.245f, metrics.Top );
-	ASSERT_EQ( 45.8f, metrics.Width );
-	ASSERT_EQ( 998.5f, metrics.Height );
-	ASSERT_EQ( 23, metrics.BidiLevel );
-	ASSERT_TRUE( metrics.IsText );
-	ASSERT_TRUE( metrics.IsTrimmed );
+	AssertHitTestMetricsMatchExpected(metrics);
 }
 
 TEST(TextLayoutTests, HitTestTextPosition)
 {
 	IDWriteTextLayoutMock mockLayout;
 	TextLayout ^layout = TextLayout::FromPointer(System::IntPtr(&mockLayout));
-	DWRITE_HIT_TEST_METRICS expectedMetrics;
-	expectedMetrics.textPosition = 0;
-	expectedMetrics.length = 1;
-	expectedMetrics.left = 0.15f;
-	expectedMetrics.top = 0.245f;
-	expectedMetrics.width = 45.8f;
-	expectedMetrics.height = 998.5f;
-	expectedMetrics.bidiLevel = 23;
-	expectedMetrics.isText = TRUE;
-	expectedMetrics.isTrimmed = TRUE;
 	EXPECT_CALL(mockLayout, HitTestTextPosition(12U, FALSE, NotNull(), NotNull(), NotNull()))
 		.Times(1)
 		.WillOnce(DoAll(SetArgumentPointee<2>(12.5f),
 		SetArgumentPointee<3>(13.5f),
-		SetArgumentPointee<4>(expectedMetrics),
+		SetArgumentPointee<4>(ExpectedHitTestMetrics()),
 		Return(S_OK)));
 	float x, y;
 	HitTestMetrics metrics = layout->HitTestTextPosition(12U, false, x, y);
+	ASSERT_TRUE( Result::Last.IsSuccess );
 	ASSERT_EQ( 12.5f, x );
 	ASSERT_EQ( 13.5f, y );
-	ASSERT_TRUE( Result::Last.IsSuccess );
-	ASSERT_EQ( 0, metrics.TextPosition );
-	ASSERT_EQ( 1, metrics.Length );
-	ASSERT_EQ( 0.15f, metrics.Left );
-	ASSERT_EQ( 0.245f, metrics.Top );
-	ASSERT_EQ( 45.8f, metrics.Width );
-	ASSERT_EQ( 998.5f, metrics.Height );
-	ASSERT_EQ( 23, metrics.BidiLevel );
-	ASSERT_TRUE( metrics.IsText );
-	ASSERT_TRUE( metrics.IsTrimmed );
+	AssertHitTestMetricsMatchExpected(metrics);
 }
