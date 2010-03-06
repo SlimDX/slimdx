a967ddb7b3a3122f70228e6f3c1da7948fe311fd
legalize@xmission.com <legalize@xmission.com@090eb97a-982f-0410-b69a-795f42cc130f>
2010-03-06 18:41:31 +0000

    DirectWrite : Implemented : TextFormat.GetLineSpacing, TextFormat.SetLineSpacing

--- tests/SlimDX.Tests/source/DirectWrite.TextFormat.Tests.cpp
+++ tests/SlimDX.Tests/source/DirectWrite.TextFormat.Tests.cpp
@@ -45,7 +45,7 @@ public:
 	MOCK_METHOD1_WITH_CALLTYPE( STDMETHODCALLTYPE, SetFlowDirection, HRESULT(DWRITE_FLOW_DIRECTION) );
 	MOCK_METHOD1_WITH_CALLTYPE( STDMETHODCALLTYPE, SetIncrementalTabStop, HRESULT(FLOAT) );
 	STDMETHOD(SetTrimming)(DWRITE_TRIMMING const* trimmingOptions, IDWriteInlineObject* trimmingSign) { return E_NOTIMPL; }
-	STDMETHOD(SetLineSpacing)(DWRITE_LINE_SPACING_METHOD lineSpacingMethod, FLOAT lineSpacing, FLOAT baseline) { return E_NOTIMPL; }
+	MOCK_METHOD3_WITH_CALLTYPE( STDMETHODCALLTYPE, SetLineSpacing, HRESULT(DWRITE_LINE_SPACING_METHOD, FLOAT, FLOAT) );
 	STDMETHOD_(DWRITE_TEXT_ALIGNMENT, GetTextAlignment)() { return DWRITE_TEXT_ALIGNMENT(-1); }
 	STDMETHOD_(DWRITE_PARAGRAPH_ALIGNMENT, GetParagraphAlignment)() { return DWRITE_PARAGRAPH_ALIGNMENT(-1); }
 	STDMETHOD_(DWRITE_WORD_WRAPPING, GetWordWrapping)() { return DWRITE_WORD_WRAPPING(-1); }
@@ -53,7 +53,7 @@ public:
 	MOCK_METHOD0_WITH_CALLTYPE( STDMETHODCALLTYPE, GetFlowDirection, DWRITE_FLOW_DIRECTION() );
 	MOCK_METHOD0_WITH_CALLTYPE( STDMETHODCALLTYPE, GetIncrementalTabStop, FLOAT() );
 	STDMETHOD(GetTrimming)(DWRITE_TRIMMING* trimmingOptions, IDWriteInlineObject** trimmingSign) { return E_NOTIMPL; }
-	STDMETHOD(GetLineSpacing)(DWRITE_LINE_SPACING_METHOD* lineSpacingMethod, FLOAT* lineSpacing, FLOAT* baseline) { return E_NOTIMPL; }
+	MOCK_METHOD3_WITH_CALLTYPE( STDMETHODCALLTYPE, GetLineSpacing, HRESULT(DWRITE_LINE_SPACING_METHOD*, FLOAT*, FLOAT*) );
 	MOCK_METHOD1_WITH_CALLTYPE( STDMETHODCALLTYPE, GetFontCollection, HRESULT(IDWriteFontCollection**) );
 	MOCK_METHOD0_WITH_CALLTYPE( STDMETHODCALLTYPE, GetFontFamilyNameLength, UINT32() );
 	MOCK_METHOD2_WITH_CALLTYPE( STDMETHODCALLTYPE, GetFontFamilyName, HRESULT(WCHAR*, UINT32) );
@@ -239,4 +239,35 @@ TEST_F(TextFormatTest, SetIncrementalTabStop)
 		.WillOnce(Return(S_OK));
 	format.Format->IncrementalTabStop = 45.5f;
 	AssertLastResultSucceeded();
+}
+
+static std::ostream &operator<<(std::ostream &stream, LineSpacingMethod method)
+{
+	return stream << method.ToString();
+}
+
+TEST_F(TextFormatTest, GetLineSpacing)
+{
+	MockedTextFormat format;
+	EXPECT_CALL(format.Mock, GetLineSpacing(NotNull(), NotNull(), NotNull()))
+		.Times(1)
+		.WillOnce(DoAll(SetArgumentPointee<0>(DWRITE_LINE_SPACING_METHOD_UNIFORM),
+						SetArgumentPointee<1>(12.5f),
+						SetArgumentPointee<2>(14.8f),
+						Return(S_OK)));
+	LineSpacingMethod method;
+	float lineSpacing, baseline;
+	ASSERT_TRUE(format.Format->GetLineSpacing(method, lineSpacing, baseline).IsSuccess);
+	ASSERT_EQ(LineSpacingMethod::Uniform, method);
+	ASSERT_EQ(12.5f, lineSpacing);
+	ASSERT_EQ(14.8f, baseline);
+}
+
+TEST_F(TextFormatTest, SetLineSpacing)
+{
+	MockedTextFormat format;
+	EXPECT_CALL(format.Mock, SetLineSpacing(DWRITE_LINE_SPACING_METHOD_UNIFORM, 12.5f, 14.8f))
+		.Times(1)
+		.WillOnce(Return(S_OK));
+	ASSERT_TRUE(format.Format->SetLineSpacing(LineSpacingMethod::Uniform, 12.5f, 14.8f).IsSuccess);
 }
