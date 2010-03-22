949f07b14074dbb97d78329c76a85e2a2e7e398d
mike.popoloski <mike.popoloski@090eb97a-982f-0410-b69a-795f42cc130f>
2010-03-22 17:39:23 +0000

    Added initial Window functionality.

---
+++ branches/v2/Products/SlimDX/Windows/WindowHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SlimDX.Windows
+{
+    class WindowHandler : NativeWindow
+    {
+        MessageHandler wndproc;
+
+        public delegate bool MessageHandler(ref Message m);
+
+        public WindowHandler(MessageHandler messageHandler)
+        {
+            wndproc = messageHandler;
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (!wndproc(ref m))
+                base.WndProc(ref m);
+        }
+    }
+}
--- branches/v2/Samples/Direct3D10/MiniTri10/Program.cs
+++ branches/v2/Samples/Direct3D10/MiniTri10/Program.cs
@@ -5,23 +5,24 @@ using SlimDX.DXGI;
 using System.Windows.Forms;
 using System.Drawing;
 using System.Reflection;
+using SlimDX.Windows;
 
-namespace MiniTri10 {
-    class Program {
-        static void Main( string[] args ) {
-            var form = new Form {
-                Text = "SlimDX - MiniTri10",
-                ClientSize = new Size( 800, 600 )
-            };
+namespace MiniTri10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var window = new Window("SlimDX - MiniTri10");
 
             var api = Loader.LoadDirect3D10();
-            IDevice10 device = null;
-            ISwapChainDXGI swapChain = null;
-            var swapChainDescription = new SwapChainDescription() {
-                BufferDescription = new DisplayModeDescription {
+            var swapChainDescription = new SwapChainDescription()
+            {
+                BufferDescription = new DisplayModeDescription
+                {
                     Format = Format.R8G8B8A8_UNORM,
-                    Width = form.ClientSize.Width,
-                    Height = form.ClientSize.Height,
+                    Width = window.ClientSize.Width,
+                    Height = window.ClientSize.Height,
                     RefreshRate = new Rational { Numerator = 60, Denominator = 1 },
                     Scaling = DisplayModeScale.Unspecified,
                     ScanlineOrdering = DisplayModeScanlineOrder.Unspecified
@@ -29,14 +30,20 @@ namespace MiniTri10 {
                 BufferCount = 1,
                 BufferUsage = Usage.RenderTargetOutput,
                 IsWindowed = true,
-                OutputWindow = form.Handle,
+                OutputWindow = window.Handle,
                 SampleDescription = new SampleDescription { Count = 1, Quality = 0 },
                 SwapEffect = SwapEffect.Discard
             };
 
-            api.CreateDeviceAndSwapChain( null, DriverType.Hardware, DeviceCreationFlags.Debug, swapChainDescription, out device, out swapChain );
+            IDevice10 device = null;
+            ISwapChainDXGI swapChain = null;
+            api.CreateDeviceAndSwapChain(null, DriverType.Hardware, DeviceCreationFlags.Debug, swapChainDescription, out device, out swapChain);
             var backbuffer = swapChain.GetBuffer<ITexture2D10>(0);
-            
+
+            MessagePump.Run(window, () =>
+            {
+            });
+
             backbuffer.Release();
         }
     }
