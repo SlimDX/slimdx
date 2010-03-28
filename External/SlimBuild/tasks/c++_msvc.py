ecdef373b4abd23c45f2a4b077c02d5f5d660b07
josh.petrie <josh.petrie@090eb97a-982f-0410-b69a-795f42cc130f>
2010-03-28 18:10:10 +0000

    Updating SlimBuild.

--- External/SlimBuild/tasks/c++_msvc.py
+++ External/SlimBuild/tasks/c++_msvc.py
@@ -1,27 +1,27 @@
 from sbuild import *
 
 def execute( build, state, **kwargs ):
-	if kwargs.has_key("include_dirs"):
-		include_paths = kwargs["include_dirs"]
-	else:
-		include_paths = []
+	output = kwargs["output"]
+
+	source_files = kwargs["source"]
 	
-	if kwargs.has_key("library_dirs"):
-		library_paths = kwargs["library_dirs"]
-	else:
-		library_paths = []
-		
-	if kwargs.has_key("libraries"):
-		libraries = kwargs["libraries"]
-	else:
-		libraries = []
+	format = kwargs.get( "format", "app" ).lower()
+	arch = kwargs.get( "arch", "x86" ).lower()
 	
-	if kwargs.has_key("assemblies"):
-		assemblies = kwargs["assemblies"]
-	else:
-		assemblies = []
-		
-	if kwargs["arch"] == "x64":
+	features = kwargs.get( "features", "" ).lower().split()
+	
+	include_paths = kwargs.get( "include_paths", [] )
+	library_paths = kwargs.get( "library_paths", [] )
+	libraries = kwargs.get( "libraries", [] )
+	
+	if "clr" in features:
+		assemblies = kwargs.get( "assemblies", [] )
+	
+	extension = "exe"
+	if format == "dll":
+		extension = "dll"
+	
+	if arch == "x64":
 		compiler = "C:/Program Files (x86)/Microsoft Visual Studio 9.0/VC/bin/amd64/cl.exe"
 		linker = "C:/Program Files (x86)/Microsoft Visual Studio 9.0/VC/bin/amd64/link.exe"
 		mt = "C:/Program Files/Microsoft SDKs/Windows/v6.0A/Bin/x64/mt.exe"
@@ -31,7 +31,7 @@ def execute( build, state, **kwargs ):
 		mt = "C:/Program Files/Microsoft SDKs/Windows/v6.0A/Bin/mt.exe"
 	
 	object_files = []
-	for source_file in kwargs["source"]:
+	for source_file in source_files:
 		object_file = FileReference( os.path.join( state.work_path, source_file.name + ".obj" ) )
 		object_files.append( object_file )
 		
@@ -39,7 +39,8 @@ def execute( build, state, **kwargs ):
 			compileCommand = [ compiler ]
 			compileCommand.append( "/nologo" )
 		
-			compileCommand.append( "/clr" )
+			if "clr" in features:
+				compileCommand.append( "/clr" )
 			compileCommand.append( "/c" )
 			
 			compileCommand.append( "/EHa" )
@@ -59,18 +60,19 @@ def execute( build, state, **kwargs ):
 			build.schedule( ScheduleItem( compileCommand ) )
 	
 	linkCommand = [ linker ]
-	linkCommand.append("/NOLOGO")
-	linkCommand.append("/DLL")
-	linkCommand.append("/INCREMENTAL:NO")
-	linkCommand.append("/DEBUG")
-	linkCommand.append("/MANIFESTFILE:%s.dll.intermediate.manifest" % kwargs["output"] )
-	linkCommand.append("/MANIFESTUAC:level='asInvoker' uiAccess='false'")
-	linkCommand.append("/MACHINE:%s" % kwargs["arch"]);
+	linkCommand.append( "/NOLOGO" )
+	if format == "dll":
+		linkCommand.append( "/DLL" )
+	linkCommand.append( "/INCREMENTAL:NO" )
+	linkCommand.append( "/DEBUG" )
+	linkCommand.append( "/MANIFESTFILE:{0}.{1}.intermediate.manifest".format( output, extension ) )
+	linkCommand.append( "/MANIFESTUAC:level='asInvoker' uiAccess='false'")
+	linkCommand.append( "/MACHINE:{0}".format( arch ) );
 	
 	for path in library_paths:
 		linkCommand.append( "/LIBPATH:{0}".format( path ) )
 	
-	linkCommand.append( "/OUT:{0}.dll".format( kwargs["output"] ) )
+	linkCommand.append( "/OUT:{0}.{1}".format( output, extension ) )
 	for object_file in object_files:
 		linkCommand.append( object_file.identity )
 	for library in libraries:
@@ -79,10 +81,8 @@ def execute( build, state, **kwargs ):
 	build.schedule( ScheduleItem( linkCommand ) )
 	
 	manifestCommand = [ mt ]
-	manifestCommand.append("-nologo")
-	manifestCommand.append("-outputresource:%s.dll;#2" % kwargs["output"] )
-	manifestCommand.append("-manifest")
-	manifestCommand.append("%s.dll.intermediate.manifest" % kwargs["output"] )
+	manifestCommand.append( "-nologo")
+	manifestCommand.append( "-outputresource:{0}.{1};#2".format( output, extension ) )
+	manifestCommand.append( "-manifest")
+	manifestCommand.append( "{0}.{1}.intermediate.manifest".format( output, extension ) )
 	build.schedule( ScheduleItem( manifestCommand ) )
-	
-
