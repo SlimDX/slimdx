a41013f8ead4fec6ffae75d36b3a7a6c850e2e6f
josh.petrie <josh.petrie@090eb97a-982f-0410-b69a-795f42cc130f>
2010-03-28 17:39:21 +0000

    Updating SlimBuild.

--- External/SlimBuild/tools/link.py
+++
@@ -1,4 +0,0 @@
-def find( options ):
-	if "x64" in options:
-		return "C:/Program Files (x86)/Microsoft Visual Studio 9.0/VC/bin/amd64/link.exe"
-	return "C:/Program Files (x86)/Microsoft Visual Studio 9.0/VC/bin/link.exe"
