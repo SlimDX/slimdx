6f543481bde3b42feccea3079578577f69df7178
josh.petrie <josh.petrie@090eb97a-982f-0410-b69a-795f42cc130f>
2010-03-28 17:39:21 +0000

    Updating SlimBuild.

--- branches/v2/External/SlimBuild/tools/g++.py
+++
@@ -1,2 +0,0 @@
-def find( options ):
-	return "g++"
--- branches/v2/External/SlimBuild/tools/mt.py
+++
@@ -1,4 +0,0 @@
-def find( options ):
-	if "x64" in options:
-		return "C:/Program Files/Microsoft SDKs/Windows/v6.0A/Bin/x64/mt.exe"
-	return "C:/Program Files/Microsoft SDKs/Windows/v6.0A/Bin/mt.exe"
