92158f078dec2032051f7c6df289a43e355ebdc1
promit.roy <promit.roy@090eb97a-982f-0410-b69a-795f42cc130f>
2010-04-06 19:11:20 +0000

    Patching the build script to work with the CI server.

--- build/scripts/build-all.bat
+++ build/scripts/build-all.bat
@@ -1,6 +1,5 @@
 CALL "%VS90COMNTOOLS%..\..\VC\vcvarsall.bat"
-
-msbuild ../slimdx.proj /m /target:build /property:Configuration=Debug;Platform=x86
-msbuild ../slimdx.proj /m /target:build /property:Configuration=Release;Platform=x86
-msbuild ../slimdx.proj /m /target:build /property:Configuration=Debug;Platform=x64
-msbuild ../slimdx.proj /m /target:build /property:Configuration=Release;Platform=x64
+msbuild %~dp0../slimdx.proj /m /target:build /property:Configuration=Debug;Platform=x86
+msbuild %~dp0../slimdx.proj /m /target:build /property:Configuration=Release;Platform=x86
+msbuild %~dp0../slimdx.proj /m /target:build /property:Configuration=Debug;Platform=x64
+msbuild %~dp0../slimdx.proj /m /target:build /property:Configuration=Release;Platform=x64
