0833fc2e0d763227aef25c8829345e8d998efee1
promit.roy <promit.roy@090eb97a-982f-0410-b69a-795f42cc130f>
2010-04-06 20:12:09 +0000

    Disabling multi-cpu MSBuilds because they break, stealing r1500.

--- build/scripts/build-all.bat
+++ build/scripts/build-all.bat
@@ -1,5 +1,5 @@
 CALL "%VS90COMNTOOLS%..\..\VC\vcvarsall.bat"
-msbuild %~dp0../slimdx.proj /m /target:build /property:Configuration=Debug;Platform=x86
-msbuild %~dp0../slimdx.proj /m /target:build /property:Configuration=Release;Platform=x86
-msbuild %~dp0../slimdx.proj /m /target:build /property:Configuration=Debug;Platform=x64
-msbuild %~dp0../slimdx.proj /m /target:build /property:Configuration=Release;Platform=x64
+msbuild %~dp0../slimdx.proj /target:build /property:Configuration=Debug;Platform=x86
+msbuild %~dp0../slimdx.proj /target:build /property:Configuration=Release;Platform=x86
+msbuild %~dp0../slimdx.proj /target:build /property:Configuration=Debug;Platform=x64
+msbuild %~dp0../slimdx.proj /target:build /property:Configuration=Release;Platform=x64
