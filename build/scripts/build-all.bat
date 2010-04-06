64453cc97031a7cc4e039ad5c5dae78004ce5eec
promit.roy <promit.roy@090eb97a-982f-0410-b69a-795f42cc130f>
2010-04-06 18:08:04 +0000

    Tweaking the build-all script.

--- build/scripts/build-all.bat
+++ build/scripts/build-all.bat
@@ -1,3 +1,5 @@
+CALL "%VS90COMNTOOLS%..\..\VC\vcvarsall.bat"
+
 msbuild ../slimdx.proj /m /target:build /property:Configuration=Debug;Platform=x86
 msbuild ../slimdx.proj /m /target:build /property:Configuration=Release;Platform=x86
 msbuild ../slimdx.proj /m /target:build /property:Configuration=Debug;Platform=x64
