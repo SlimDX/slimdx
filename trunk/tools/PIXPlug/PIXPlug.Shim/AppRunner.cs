407cb213aadfe10cd136375624dbc22a76b48097
mike.popoloski <mike.popoloski@090eb97a-982f-0410-b69a-795f42cc130f>
2010-05-03 21:30:05 +0000

    Adding PIXPlug utility to run x64 SlimDX applications in PIX.

---
+++ trunk/tools/PIXPlug/PIXPlug.Shim/AppRunner.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PIXPlug.Shim
+{
+    public static class AppRunner
+    {
+        public static int Start(string path)
+        {
+            return AppDomain.CurrentDomain.ExecuteAssembly(path);
+        }
+    }
+}
