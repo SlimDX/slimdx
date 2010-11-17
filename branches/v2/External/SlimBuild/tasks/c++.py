7eec8cea82d7d88b6f5b8664290cde4e1626aee5
josh.petrie <josh.petrie@090eb97a-982f-0410-b69a-795f42cc130f>
2010-11-17 01:47:14 +0000

    Removed tests, external dependencies.

--- branches/v2/External/SlimBuild/tasks/c++.py
+++
@@ -1,8 +0,0 @@
-from sbuild import *
-
-def execute( build, state, **kwargs ):
-	if build.env.hostPlatform == Environment.WINDOWS:
-		build( "c++_msvc", **kwargs )
-	else:
-		build( "c++_g++", **kwargs )
-	
--- branches/v2/External/SlimBuild/tasks/slimbuild.py
+++
@@ -1,6 +0,0 @@
-from sbuild import *
-
-def execute( build, state, **kwargs ):
-	script = kwargs["script"]
-	build.run( script )
-	
