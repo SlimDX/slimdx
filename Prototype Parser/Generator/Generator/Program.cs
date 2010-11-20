20541e7ce1ee6932970175dfa76349fb9d8da374
mike.popoloski <mike.popoloski@090eb97a-982f-0410-b69a-795f42cc130f>
2010-11-20 23:56:48 +0000

    Refactored the preprocessor.

--- Prototype Parser/Generator/Generator/Program.cs
+++ Prototype Parser/Generator/Generator/Program.cs
@@ -87,78 +87,30 @@ namespace Generator
 				}
 			}
 
-			// primary source file is the one that wave is run against to produce
-			// a single preprocessed monolithic header
-			var primarySource = options.GetOption("PrimarySource");
-			var grammarPath = options.GetOption("Grammar");
-			var wavePath = options.GetOption("Wave");
-
 			// run boost::wave on the primary source file to get a preprocessed file and a list of macros
-			// -E indicates default naming scheme for output file (ie. input.i)
-			// -m macros.txt creates a separate file containing a list of all found macros
-			// --variadics enables macros with empty argument lists (necessary for some win32 header)
-			// -S adds an include path (specified in our config file)
-			// -D adds a predefined macro (several are necessary to compile the win32 headers)
-			// final argument is the primary header file to run against
-			var arguments = new StringBuilder("-E -m macros.txt --variadics ");
-
-			foreach (var path in options.GetOptions("IncludePaths"))
-				arguments.Append("-S \"").Append(path).Append("\" ");
-
-			foreach (var symbol in options.GetOptions("Symbols"))
-				arguments.Append("-D ").Append(symbol).Append(" ");
-
-			// copy the primary source file to local so that wave doesn't try to create the output file
-			// in the source directory, which potentially has restricted access
-			var source = Path.GetFileName(primarySource);
-			File.Copy(primarySource, source, true);
-
-			arguments.AppendFormat("\"{0}\"", source);
-			Preprocess(wavePath, arguments.ToString());
-			File.Delete(source);
+			var preprocessor = new Preprocessor(options);
+			Console.WriteLine(preprocessor.Run());
 
 			// before parsing, run some transformations on the preprocessed file to
-			// get rid of garbage to cut down on the size needed to be examined
-			// as well as to get rid of junk I was too lazy to add to the parser grammar.
+			// to cut down on the size needed to be examined as well as to get rid of
+			// junk I was too lazy to add to the parser grammar.
 			// this includes dropping any source that is not from the given primary or ancillary
 			// sources, which is indicated in the preprocessed file by #line directives
 			List<string> relevantSources;
 			if (!options.TryGetValue("AncillarySources", out relevantSources))
 				relevantSources = new List<string>();
 
+			var source = preprocessor.Source;
 			relevantSources.Add(Path.Combine(Directory.GetCurrentDirectory(), source));
 			source = Path.ChangeExtension(source, ".i");
-			Transform(source, new HashSet<string>(relevantSources));
+			PreTransform(source, new HashSet<string>(relevantSources));
 
 			// run the parse on the preprocessed file to generate a model of the file in memory
-			var parser = new HeaderParser(grammarPath);
+			var parser = new HeaderParser(options.GetOption("Grammar"));
 			var root = parser.Parse(source);
 
 			// for testing purposes, output XML of the parse tree
 			root.ToXml().Save("test.xml");
-		}
-
-		/// <summary>
-		/// Run boost::wave on the source headers to produce a single preprocessed output file.
-		/// </summary>
-		/// <param name="wavePath">The path to the wave executable.</param>
-		/// <param name="arguments">The arguments passed to boost::wave.</param>
-		static void Preprocess(string wavePath, string arguments)
-		{
-			var startInfo = new ProcessStartInfo(wavePath, arguments)
-			{
-				CreateNoWindow = true,
-				RedirectStandardOutput = true,
-				UseShellExecute = false
-			};
-
-			using (var process = Process.Start(startInfo))
-			{
-				string info = process.StandardOutput.ReadToEnd();
-				process.WaitForExit();
-
-				Console.WriteLine(info);
-			}
 		}
 
 		/// <summary>
@@ -167,7 +119,7 @@ namespace Generator
 		/// <param name="preprocessedFile">The preprocessed file.</param>
 		/// <param name="relevantSources">The relevant sources. 
 		/// Any code that did not originate in one of these sources is removed before parsing.</param>
-		static void Transform(string preprocessedFile, HashSet<string> relevantSources)
+		static void PreTransform(string preprocessedFile, HashSet<string> relevantSources)
 		{
 			var output = new StringBuilder();
 			bool keepSource = false;
