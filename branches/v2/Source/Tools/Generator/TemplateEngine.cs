c3db0055dcf7a06b3773667d2c35e559c46e23da
josh.petrie <josh.petrie@090eb97a-982f-0410-b69a-795f42cc130f>
2010-12-12 18:58:11 +0000

    Added support for callback tokens for specifying arbitrarily complex replacements during template evaluation.

--- branches/v2/Source/Tools/Generator/TemplateEngine.cs
+++ branches/v2/Source/Tools/Generator/TemplateEngine.cs
@@ -18,13 +18,13 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+using System.Collections;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.IO;
-using System.Text.RegularExpressions;
-using System.Collections;
+using System.Diagnostics.Contracts;
 
 namespace SlimDX.Generator
 {
@@ -61,7 +61,7 @@ namespace SlimDX.Generator
 		/// Applies the specified template to the given source object.
 		/// </summary>
 		/// <param name="template">The text template.</param>
-		/// <param name="source">The source object from which properties will be taken to insert into the template.</param>
+		/// <param name="source">The object used as a data source during evaluation.</param>
 		/// <returns>The generated string.</returns>
 		public string Apply(string template, object source)
 		{
@@ -73,60 +73,103 @@ namespace SlimDX.Generator
 			return regex.Replace(template.Replace("\\{", "`"), m => Evaluate(source, m)).Replace("`", "{").Replace("\\}", "}");
 		}
 
+		/// <summary>
+		/// Registers a callback to be invoked when evaluating a callback token.
+		/// </summary>
+		/// <param name="token">The token name.</param>
+		/// <param name="callback">The callback to invoke.</param>
+		public void RegisterCallback(string token, Func<object, string> callback)
+		{
+			if (string.IsNullOrEmpty(token))
+				throw new ArgumentException("Value may not be null or empty.", "token");
+			if (callback == null)
+				throw new ArgumentNullException("callback");
+
+			callbacks[token] = callback;
+		}
+
+		const string callbackGlyph = "@";
+		const string recursionGlyph = ":";
+
+		Dictionary<string, Func<object, string>> callbacks = new Dictionary<string, Func<object, string>>();
+
 		string Evaluate(object source, Match match)
 		{
-			// grab the property name tag and remove the enclosing brackets
-			// {foo} -> foo
+			if (match.Captures.Count <= 0)
+				throw new InvalidOperationException("No captures in match.");
+
+			// Brackets are stripped from the token for easier processing.
 			var capture = match.Captures[0].Value.Trim('{', '}');
-			string propertyName = capture;
-
-			if (propertyName == "Namespace")
-				return Namespace;
-
-			// if the name has a colon, it indicates that the type has another template applied to it
-			int colon = capture.IndexOf(':');
-			if (colon >= 0)
-				propertyName = capture.Substring(0, colon);
-
-			// if the name has a period, it indicates a sub-property of the element
-			int period = propertyName.IndexOf('.');
-			string subName = null;
-			if (period >= 0)
+			if (capture.StartsWith(callbackGlyph))
 			{
-				subName = propertyName.Substring(period + 1);
-				propertyName = propertyName.Substring(0, period);
-			}
-
-			// get the value of the property from the source object
-			var value = source.GetType().GetProperty(propertyName).GetValue(source, null);
-			if (subName != null)
-				value = value.GetType().GetProperty(subName).GetValue(value, null);
-
-			if (colon >= 0)
-			{
-				// extract the new template name
-				// {Foo:lol.txt} -> lol.txt
-				var template = capture.Substring(colon + 1);
-				var suffix = string.Empty;
-				
-				int space = template.IndexOf(' ');
-				if (space >= 0)
-				{
-					// the suffix is a list of characters to apply after each element
-					// in the enumeration
-					suffix = Escape(template.Substring(space + 1));
-					template = template.Substring(0, space);
-				}
-
-				var enumerable = GetEnumerable(value);
-				if (enumerable != null)
-					return string.Join(suffix, enumerable.Cast<object>().Select(o => Apply(template, o)));
-
-				// the element is not enumerable, so just apply the new template to the value
-				return Apply(template, value);
+				var callbackName = capture.Substring(callbackGlyph.Length);
+				return EvaluateCallback(callbackName, source);
 			}
 			else
-				return Format(value);
+			{
+				string propertyName = capture;
+
+				if (propertyName == "Namespace")
+					return Namespace;
+
+				// if the name has a colon, it indicates that the type has another template applied to it
+				int colon = capture.IndexOf(':');
+				if (colon >= 0)
+					propertyName = capture.Substring(0, colon);
+
+				// if the name has a period, it indicates a sub-property of the element
+				int period = propertyName.IndexOf('.');
+				string subName = null;
+				if (period >= 0)
+				{
+					subName = propertyName.Substring(period + 1);
+					propertyName = propertyName.Substring(0, period);
+				}
+
+				// get the value of the property from the source object
+				var value = source.GetType().GetProperty(propertyName).GetValue(source, null);
+				if (subName != null)
+					value = value.GetType().GetProperty(subName).GetValue(value, null);
+
+				if (colon >= 0)
+				{
+					// extract the new template name
+					// {Foo:lol.txt} -> lol.txt
+					var template = capture.Substring(colon + 1);
+					var suffix = string.Empty;
+
+					int space = template.IndexOf(' ');
+					if (space >= 0)
+					{
+						// the suffix is a list of characters to apply after each element
+						// in the enumeration
+						suffix = Escape(template.Substring(space + 1));
+						template = template.Substring(0, space);
+					}
+
+					var enumerable = GetEnumerable(value);
+					if (enumerable != null)
+						return string.Join(suffix, enumerable.Cast<object>().Select(o => Apply(template, o)));
+
+					// the element is not enumerable, so just apply the new template to the value
+					return Apply(template, value);
+				}
+				else
+					return Format(value);
+			}
+		}
+
+		/// <summary>
+		/// Processes a callback token within a template during evalution.
+		/// </summary>
+		/// <param name="callbackName">The name of the callback to invoke.</param>
+		/// <param name="source">The object used as a data source during evaluation.</param>
+		string EvaluateCallback(string callbackName, object source)
+		{
+			Func<object, string> callback = null;
+			if (!callbacks.TryGetValue(callbackName, out callback))
+				throw new InvalidOperationException(string.Format("No callback registered for '{0}.'", callbackName));
+			return callback(source);
 		}
 
 		static string Format(object source)
