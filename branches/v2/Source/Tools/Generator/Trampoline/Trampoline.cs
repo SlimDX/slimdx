2a76158c1b40a5a91ea18f139e7c847477d18872
josh.petrie <josh.petrie@090eb97a-982f-0410-b69a-795f42cc130f>
2010-12-12 07:33:51 +0000

    Equals/GetHashCode/ToString overloads.

--- branches/v2/Source/Tools/Generator/Trampoline/Trampoline.cs
+++ branches/v2/Source/Tools/Generator/Trampoline/Trampoline.cs
@@ -90,6 +90,53 @@ namespace SlimDX.Generator
 			private set;
 		}
 
+		/// <summary>
+		/// Determines whether the specified <see cref="System.Object"/> is equal to this instance.
+		/// </summary>
+		/// <param name="other">The <see cref="System.Object"/> to compare with this instance.</param>
+		/// <returns>
+		/// 	<c>true</c> if the specified <see cref="System.Object"/> is equal to this instance; otherwise, <c>false</c>.
+		/// </returns>
+		public override bool Equals(object other)
+		{
+			var trampoline = other as Trampoline;
+			if (trampoline == null || trampoline.ReturnType != ReturnType || trampoline.ManagedParameterTypes.Count != ManagedParameterTypes.Count)
+				return false;
+
+			for (var parameterIndex = 0; parameterIndex < trampoline.ManagedParameterTypes.Count; ++parameterIndex)
+			{
+				if (trampoline.ManagedParameterTypes[parameterIndex].Type != ManagedParameterTypes[parameterIndex].Type)
+					return false;
+			}
+
+			return true;
+		}
+
+		/// <summary>
+		/// Returns a hash code for this instance.
+		/// </summary>
+		/// <returns>
+		/// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table. 
+		/// </returns>
+		public override int GetHashCode()
+		{
+			var result = ReturnType.GetHashCode();
+			foreach (var type in ManagedParameterTypes)
+				result += type.Type.GetHashCode();
+
+			return result;
+		}
+
+		/// <summary>
+		/// Returns a <see cref="System.String"/> that represents this instance.
+		/// </summary>
+		/// <returns>
+		/// A <see cref="System.String"/> that represents this instance.
+		/// </returns>
+		public override string ToString()
+		{
+			return string.Format("{0} -> {1}", ReturnType, string.Join(", ", ManagedParameterTypes));
+		}
 		#endregion
 	}
 }
