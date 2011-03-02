9107785411b07ae547af99b584ed2b34f262656f
mike.popoloski <mike.popoloski@090eb97a-982f-0410-b69a-795f42cc130f>
2011-03-02 01:47:47 +0000

    Added SwapChainEx to D3D9. Resolves issue 763.

--- trunk/source/direct3d9/SwapChain.h
+++ trunk/source/direct3d9/SwapChain.h
@@ -29,12 +29,16 @@ namespace SlimDX
 	{
 		ref class PresentParameters;
 		ref class Surface;
+		ref class Device;
 
 		/// <summary>Applications use the methods of the SwapChain9 class to manipulate a swap chain.</summary>
 		/// <unmanaged>IDirect3DSwapChain9</unmanaged>
 		public ref class SwapChain : public ComObject
 		{
 			COMOBJECT(IDirect3DSwapChain9, SwapChain);
+
+		private protected:
+			SwapChain();
 
 		public:
 			SwapChain( SlimDX::Direct3D9::Device^ device, PresentParameters^ presentParameters );
