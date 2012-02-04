47f5c216c5ca231a2cdd71f182001841fe536ccf
promit.roy <promit.roy@090eb97a-982f-0410-b69a-795f42cc130f>
2012-02-04 23:25:47 +0000

    Website updates for Jan 12 release.

--- website/download_jan12.php
+++ website/download_jan12.php
@@ -1,5 +1,5 @@
 <?php
-	$g_pageTitle = "SlimDX Download Page (September 2011)";
+	$g_pageTitle = "SlimDX Download Page (January 2012)";
 	$g_pageName = "Download";
 	
 	require_once("header.php");
@@ -8,38 +8,50 @@
 <div id="content">
 	<div class="wrapper">
 		<div id="left">
-			<h1>September 2011</h1>
+			<h1>January 2012</h1>
 			<div class="paragraph">
 				<h2>Introduction</h2>
         <p>
-          The September 2011 release of SlimDX is now out!
+          The January 2012 release of SlimDX is now out!
         </p>
         <br />
-				<p>
-          The September 2011 release was a minor stability release.
+		<p>
+          The January 2012 release was a minor stability release.
           Once again this release does not reflect a corresponding release of the DirectX SDK.
-          While several improvements were made, along with internal upgrades to the SlimDX website, server,
-          and documentation system, the majority of development effort has been focused on the V2 branch,
-          which was recently moved to GitHub.
+          Development effort was spent on fixing minor bugs and resolving issues in the installer and deployment infrastructure. 
+        </p>
+        <p>
+          <font color="red">NOTE:</font> As of this release, samples are no longer part of the SDK but shipped as a separate download.
         </p><br />
 			</div>
 			
 			<div class="paragraph">
-				<h2>End User Runtime</h2>
+			<h2>End User Runtime</h2>
+			<p><font color="red">NOTE:</font> Application developers do <b>not</b> need these files.</p>
         <p>
-          This is the package for end users.
-          It works on both 32 and 64 bit systems, for all versions of Windows supported by the .NET Framework. Pick the one that matches your target framework.
+          These packages are for end users and install all prerequisites for SlimDX based software.
+          If you're just trying to run a SlimDX based program that someone else gave you, you will need one of these installers.
+          Developers do not need this except for distribution purposes, and should install the SDK below instead.
+          The SDK includes these files.
         </p>
         <br />
         <p>
-        If you're just trying to run a SlimDX program that someone else gave you, you will need one of these installers.
-				Developers should point users to one of these links, or include it with their own installers.
-				However, developers should install the SDK below, not this package.
-				See the documentation for more information.
-				</p><br />
-				<a href="http://slimdx.googlecode.com/files/SlimDX%20Runtime%20for%20.NET%202.0%20%28September%202011%29.msi"><img src="images/install_20.png" alt=".NET 2.0 User Runtime" /></a>
-        <!-- <a href="http://slimdx.googlecode.com/files/SlimDX%20Runtime%20for%20.NET%204.0%20%28March%202011%29.exe"><img src="images/install_40.png" alt=".NET 4.0 User Runtime" /></a> -->
-        <br />The 4.0 runtime is not available for this version. It will be available with a December 2011 update.
+        <table class="simple_table">
+          <tr>
+            <td style="border: 0;"></td>
+            <td style="font-weight: bold;">x86 (32-bit)</td>
+            <td style="font-weight: bold;">x64 (64-bit)</td>
+          </tr>
+          <tr>
+            <td style="text-align: center; font-weight: bold;">.NET 2.0</td>
+            <td colspan="2"><a href="http://slimdx.googlecode.com/files/SlimDX%20Runtime%20.NET%202.0%20%28January%202012%29.msi">Download</td>
+          </tr>
+          <tr>
+            <td style="font-weight: bold;">.NET 4.0</td>
+            <td><a href="http://slimdx.googlecode.com/files/SlimDX%20Runtime%20.NET%204.0%20x86%20%28January%202012%29.msi">x86 Download</a></td>
+            <td><a href="http://slimdx.googlecode.com/files/SlimDX%20Runtime%20.NET%204.0%20x64%20%28January%202012%29.msi">x64 Download</a></td>
+          </tr>
+        </table>
 			</div>
 			
 			<div class="paragraph">
@@ -47,9 +59,8 @@
 				<p>The SDK includes all of the binaries, documentation, and samples.
 				It also includes all of the files necessary to build custom installation packages if desired.
 				The SDK supports both 32 bit and 64 bit systems.
-				Note that for the September 2011 SDK release, Visual C++ runtimes are not included and must be installed separately.
 				</p><br />
-				<a href="http://slimdx.googlecode.com/files/SlimDX%20SDK%20%28September%202011%29.msi"><img src="images/install_sdk.png" alt="Latest Developer SDK" /></a>
+				<a href="http://code.google.com/p/slimdx/downloads/detail?name=SlimDX%20SDK%20%28January%202012%29.msi"><img src="images/install_sdk.png" alt="Latest Developer SDK" /></a>
 			</div>
 			
 			<div class="paragraph">
@@ -58,7 +69,7 @@
 				It should help get you moving.
 				Other pages of interest include:</p>
 				<ul>
-				<li><a href="http://slimdx.org/docs/#What's_New_In_September_2011" rel="nofollow">What&#x27;s Changed in the September 2011 SDK</a> -- Information on the work we did for the September 2011 SDK release.</li>
+				<li><a href="http://slimdx.org/docs/#What's_New_In_January_2012" rel="nofollow">What&#x27;s Changed in the January 2012 SDK</a> -- Information on the work we did for the January 2012 SDK release.</li>
 				<li><a href="http://slimdx.org/" rel="nofollow">SlimDX Main Page</a> -- Our main homepage. This page makes it fairly straightforward to get to all of the other pages related to this project.</li>
 				<li><a href="http://slimdx.org/docs/#Managed_Message_Loop" rel="nofollow">Managed Message Loop</a> -- Describes the rationale behind the recommended message loop for SlimDX applications.</li>
 				<li><a href="http://slimdx.org/docs/#Debugging_Tips" rel="nofollow">Debugging Tips</a> -- Tips on how to work on and debug applications using SlimDX. It&#x27;s a really good idea to be familiar with the contents of this page, and check on it every so often, because it is updated periodically.</li>
@@ -77,7 +88,7 @@
 	
 		<div id="right">
 			<ul class="sidemenu">
-				<li><a href="http://slimdx.org/docs/#What's_New_In_September_2011">Release Notes</a></li>
+				<li><a href="http://slimdx.org/docs/#What's_New_In_January_2012">Release Notes</a></li>
 				<li><a href="http://slimdx.org/docs">Documentation</a></li>
 				<li><a href="license.php">Licensing Information</a></li>
 				<li><a href="releases.php">Previous Releases</a></li>
--- website/releases.php
+++ website/releases.php
@@ -11,7 +11,9 @@
 			<div class="paragraph">
 				<h2>Previous Releases</h2>
 				<ul>
-          <li><b><a href="download_mar11.php">March 2011</a> -- version 11.43.</b></li>
+				<li><b><a href="download_jan12.php">January 2012</a> -- version 13.43.</b></li>
+				<li><a href="download_sep11.php">September 2011</a> -- version 12.43.</li>
+          <li><a href="download_mar11.php">March 2011</a> -- version 11.43.</li>
           <li><a href="download_june10.php">June 2010</a> -- version 10.43.</li>
           <li><a href="download_feb10.php">Feburary 2010</a> -- version 9.42.</li>
 					<li><a href="download_aug09.php">August 2009</a> -- version 8.42.</li>
