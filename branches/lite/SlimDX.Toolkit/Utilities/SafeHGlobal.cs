2ea3fa61808db78a945b180ded6a6d9f9351ea97
mike.popoloski <mike.popoloski@090eb97a-982f-0410-b69a-795f42cc130f>
2012-03-19 21:40:29 +0000

    More work on the SpriteBatch support.

---
+++ branches/lite/SlimDX.Toolkit/Utilities/SafeHGlobal.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace SlimDX.Toolkit
+{
+    /// <summary>
+    /// Provides a safe handle around a block of unmanaged memory.
+    /// </summary>
+    public class SafeHGlobal : SafeHandle
+    {
+        /// <summary>
+        /// When overridden in a derived class, gets a value indicating whether the handle value is invalid.
+        /// </summary>
+        /// <returns>true if the handle value is invalid; otherwise, false.</returns>
+        ///   
+        /// <PermissionSet>
+        ///   <IPermission class="System.Security.Permissions.SecurityPermission, mscorlib, Version=2.0.3600.0, Culture=neutral, PublicKeyToken=b77a5c561934e089" version="1" Flags="UnmanagedCode"/>
+        ///   </PermissionSet>
+        public override bool IsInvalid
+        {
+            get { return handle == IntPtr.Zero; }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SafeHGlobal"/> class.
+        /// </summary>
+        /// <param name="sizeInBytes">The size of the block of memory to allocate, in bytes.</param>
+        public SafeHGlobal(int sizeInBytes)
+            : base(Marshal.AllocHGlobal(sizeInBytes), true)
+        {
+        }
+
+        /// <summary>
+        /// When overridden in a derived class, executes the code required to free the handle.
+        /// </summary>
+        /// <returns>
+        /// true if the handle is released successfully; otherwise, in the event of a catastrophic failure, false. In this case, it generates a releaseHandleFailed MDA Managed Debugging Assistant.
+        /// </returns>
+        protected override bool ReleaseHandle()
+        {
+            Marshal.FreeHGlobal(handle);
+            return true;
+        }
+    }
+}
